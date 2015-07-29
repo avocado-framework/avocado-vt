avocado-framework/avocado-vt

commit 7781ad38414d822f4f13b824029519ed7b3ec5d1

virt-test: Remove dependencies on autotest
Let's remove dependencies on autotest as much as
possible, except in two instances where autotest
can't be replaced right now:

1) utils_test.run_autotest()
2) utils_test.qemu.MultiHostMigrationTest()

In such cases, let's restrict the autotest imports
to those specific functions, so the rest of the
suite can work without an autotest install.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

virttest/asset.py
@@ -1,11 +1,190 @@
 import urllib2
 import logging
 import os
-import glob
-from autotest.client import utils, test_config
-from autotest.client.shared import git, error
-import data_dir
 import re
+import string
+import types
+import glob
+import ConfigParser
+import StringIO
+
+from avocado.utils import process
+from avocado.utils import genio
+from avocado.utils import crypto
+from avocado.utils import download
+from avocado.utils import git
+
+from . import data_dir
+
+
+class ConfigLoader:
+
+    """
+    Base class of the configuration parser
+    """
+
+    def __init__(self, cfg, tmpdir='/tmp', raise_errors=False):
+        """
+        Instantiate ConfigParser and provide the file like object that we'll
+        use to read configuration data from.
+        :param cfg: Where we'll get configuration data. It can be either:
+                * A URL containing the file
+                * A valid file path inside the filesystem
+                * A string containing configuration data
+        :param tmpdir: Where we'll dump the temporary conf files.
+        :param raise_errors: Whether config value absences will raise
+                ValueError exceptions.
+        """
+        # Base Parser
+        self.parser = ConfigParser.ConfigParser()
+        # Raise errors when lacking values
+        self.raise_errors = raise_errors
+        # File is already a file like object
+        if hasattr(cfg, 'read'):
+            self.cfg = cfg
+            self.parser.readfp(self.cfg)
+        elif isinstance(cfg, types.StringTypes):
+            # Config file is a URL. Download it to a temp dir
+            if cfg.startswith('http') or cfg.startswith('ftp'):
+                self.cfg = os.path.join(tmpdir, os.path.basename(cfg))
+                download.url_download(cfg, self.cfg)
+                self.parser.read(self.cfg)
+            # Config is a valid filesystem path to a file.
+            elif os.path.exists(os.path.abspath(cfg)):
+                if os.path.isfile(cfg):
+                    self.cfg = os.path.abspath(cfg)
+                    self.parser.read(self.cfg)
+                else:
+                    e_msg = 'Invalid config file path: %s' % cfg
+                    raise IOError(e_msg)
+            # Config file is just a string, convert it to a python file like
+            # object using StringIO
+            else:
+                self.cfg = StringIO.StringIO(cfg)
+                self.parser.readfp(self.cfg)
+
+    def get(self, section, option, default=None):
+        """
+        Get the value of a option.
+
+        Section of the config file and the option name.
+        You can pass a default value if the option doesn't exist.
+
+        :param section: Configuration file section.
+        :param option: Option we're looking after.
+        :default: In case the option is not available and raise_errors is set
+                to False, return the default.
+        """
+        if not self.parser.has_option(section, option):
+            if self.raise_errors:
+                raise ValueError('No value for option %s. Please check your '
+                                 'config file "%s".' % (option, self.cfg))
+            else:
+                return default
+
+        return self.parser.get(section, option)
+
+    def set(self, section, option, value):
+        """
+        Set an option.
+
+        This change is not persistent unless saved with 'save()'.
+        """
+        if not self.parser.has_section(section):
+            self.parser.add_section(section)
+        return self.parser.set(section, option, value)
+
+    def remove(self, section, option):
+        """
+        Remove an option.
+        """
+        if self.parser.has_section(section):
+            self.parser.remove_option(section, option)
+
+    def save(self):
+        """
+        Save the configuration file with all modifications
+        """
+        if not self.cfg:
+            return
+        fileobj = file(self.cfg, 'w')
+        try:
+            self.parser.write(fileobj)
+        finally:
+            fileobj.close()
+
+    def check(self, section):
+        """
+        Check if the config file has valid values
+        """
+        if not self.parser.has_section(section):
+            return False, "Section not found: %s" % (section)
+
+        options = self.parser.items(section)
+        for i in range(options.__len__()):
+            param = options[i][0]
+            aux = string.split(param, '.')
+
+            if aux.__len__ < 2:
+                return False, "Invalid parameter syntax at %s" % (param)
+
+            if not self.check_parameter(aux[0], options[i][1]):
+                return False, "Invalid value at %s" % (param)
+
+        return True, None
+
+    def check_parameter(self, param_type, parameter):
+        """
+        Check if a option has a valid value
+        """
+        if parameter == '' or parameter is None:
+            return False
+        elif param_type == "ip" and self.__isipaddress(parameter):
+            return True
+        elif param_type == "int" and self.__isint(parameter):
+            return True
+        elif param_type == "float" and self.__isfloat(parameter):
+            return True
+        elif param_type == "str" and self.__isstr(parameter):
+            return True
+
+        return False
+
+    def __isipaddress(self, parameter):
+        """
+        Verify if the ip address is valid
+
+        :param ip String: IP Address
+        :return: True if a valid IP Address or False
+        """
+        octet1 = "([1-9][0-9]{,1}|1[0-9]{2}|2[0-4][0-9]|25[0-5])"
+        octet = "([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])"
+        pattern = "^" + octet1 + "\.(" + octet + "\.){2}" + octet + "$"
+        if re.match(pattern, parameter) is None:
+            return False
+        else:
+            return True
+
+    def __isint(self, parameter):
+        try:
+            int(parameter)
+        except Exception, e_stack:
+            return False
+        return True
+
+    def __isfloat(self, parameter):
+        try:
+            float(parameter)
+        except Exception, e_stack:
+            return False
+        return True
+
+    def __isstr(self, parameter):
+        try:
+            str(parameter)
+        except Exception, e_stack:
+            return False
+        return True
 
 
 def get_known_backends():
@@ -74,7 +253,7 @@ def get_test_provider_info(provider):
     provider_info = {}
     provider_path = os.path.join(data_dir.get_test_providers_dir(),
                                  '%s.ini' % provider)
-    provider_cfg = test_config.config_loader(provider_path)
+    provider_cfg = ConfigLoader(provider_path)
     provider_info['name'] = provider
     provider_info['uri'] = provider_cfg.get('provider', 'uri')
     provider_info['branch'] = provider_cfg.get('provider', 'branch', 'master')
@@ -120,12 +299,12 @@ def download_test_provider(provider, update=False):
                                         destination_dir=download_dst)
             os.chdir(download_dst)
             try:
-                utils.run('git remote add origin %s' % uri)
-            except error.CmdError:
+                process.run('git remote add origin %s' % uri)
+            except process.CmdError:
                 pass
-            utils.run('git pull origin %s' % branch)
+            process.run('git pull origin %s' % branch)
         os.chdir(download_dst)
-        utils.system('git log -1')
+        process.system('git log -1')
         os.chdir(original_dir)
 
 
@@ -173,7 +352,7 @@ def get_file_asset(title, src_path, destination):
 def get_asset_info(asset):
     asset_info = {}
     asset_path = os.path.join(data_dir.get_download_dir(), '%s.ini' % asset)
-    asset_cfg = test_config.config_loader(asset_path)
+    asset_cfg = ConfigLoader(asset_path)
 
     asset_info['url'] = asset_cfg.get(asset, 'url')
     asset_info['sha1_url'] = asset_cfg.get(asset, 'sha1_url')
@@ -225,7 +404,7 @@ def uncompress_asset(asset_info, force=False):
 
         if os.path.isfile(destination) and force:
             os.chdir(os.path.dirname(destination_uncompressed))
-            utils.run(uncompress_cmd)
+            # process.run(uncompress_cmd)
 
 
 def download_file(asset_info, interactive=False, force=False):
@@ -266,12 +445,12 @@ def download_file(asset_info, interactive=False, force=False):
     if not os.path.isfile(destination):
         logging.warning("File %s not found", destination)
         if interactive:
-            answer = utils.ask("Would you like to download it from %s?" % url)
+            answer = genio.ask("Would you like to download it from %s?" % url)
         else:
             answer = 'y'
         if answer == 'y':
-            utils.interactive_download(
-                url, destination, "Downloading %s" % title)
+            download.url_download_interactive(url, destination,
+                                              "Downloading %s" % title)
             had_to_download = True
         else:
             logging.warning("Missing file %s", destination)
@@ -283,11 +462,11 @@ def download_file(asset_info, interactive=False, force=False):
             answer = 'y'
 
         if answer == 'y':
-            actual_sha1 = utils.hash_file(destination, method='sha1')
+            actual_sha1 = crypto.hash_file(destination, algorithm='sha1')
             if actual_sha1 != sha1:
                 logging.info("Actual SHA1 sum: %s", actual_sha1)
                 if interactive:
-                    answer = utils.ask("The file seems corrupted or outdated. "
+                    answer = genio.ask("The file seems corrupted or outdated. "
                                        "Would you like to download it?")
                 else:
                     logging.info("The file seems corrupted or outdated")
@@ -295,14 +474,15 @@ def download_file(asset_info, interactive=False, force=False):
                 if answer == 'y':
                     logging.info("Updating image to the latest available...")
                     while not file_ok:
-                        utils.interactive_download(url, destination, title)
-                        sha1_post_download = utils.hash_file(destination,
-                                                             method='sha1')
+                        download.url_download_interactive(url, destination,
+                                                          title)
+                        sha1_post_download = crypto.hash_file(destination,
+                                                              algorithm='sha1')
                         had_to_download = True
                         if sha1_post_download != sha1:
                             logging.error("Actual SHA1 sum: %s", actual_sha1)
                             if interactive:
-                                answer = utils.ask("The file downloaded %s is "
+                                answer = genio.ask("The file downloaded %s is "
                                                    "corrupted. Would you like "
                                                    "to try again?" %
                                                    destination)
@@ -365,7 +545,7 @@ def download_asset(asset, interactive=True, restore_image=False):
     destination = asset_info['destination']
 
     if (interactive and not os.path.isfile(destination)):
-        answer = utils.ask("File %s not present. Do you want to download it?" %
+        answer = genio.ask("File %s not present. Do you want to download it?" %
                            asset_info['title'])
     else:
         answer = "y"

commit e2dd4f346dedf567286a84b9774525e61bb78462

virttest.asset: Fix bug introduced in 7781ad3
Uncomment process.system call, introduced in the
aforementioned commit.  Turns out process.system()
and friends have a funny interaction with our
InterruptedThread, and the thread will quickly
report as having finished, when it's in fact still
working. Let's work around this by using the
commands API.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

virttest/asset.py
@@ -7,6 +7,7 @@ import types
 import glob
 import ConfigParser
 import StringIO
+import commands
 
 from avocado.utils import process
 from avocado.utils import genio
@@ -404,7 +405,9 @@ def uncompress_asset(asset_info, force=False):
 
         if os.path.isfile(destination) and force:
             os.chdir(os.path.dirname(destination_uncompressed))
-            # process.run(uncompress_cmd)
+            logging.debug('Uncompressing %s -> %s', destination,
+                          destination_uncompressed)
+            commands.getstatusoutput(uncompress_cmd)
 
 
 def download_file(asset_info, interactive=False, force=False):