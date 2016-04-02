3348338ed08c673d4e69e259b267ab67fe0cd67c
Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>
2016-04-03 00:09:50 +0530

    virttest.defaults: Added support for default machine type based on arch
    Added support for choosing default machine type
    based on machine architecture

--- virttest/defaults.py
+++ virttest/defaults.py
@@ -1,9 +1,22 @@
+import platform
+ARCH = platform.machine()
+# Set the default machine to i440fx for unidentifed arch also
+# Below line can be removed once support for all possible arch is covered
 DEFAULT_MACHINE_TYPE = "i440fx"
+
+if ARCH in ('ppc64', 'ppc64le'):
+    DEFAULT_MACHINE_TYPE = "pseries"
+elif ARCH in ('x86_64', 'i386'):
+    DEFAULT_MACHINE_TYPE = "i440fx"
+else:
+    # TODO: Handle other supported archs
+    pass
 
 
 def get_default_guest_os_info():
     """
     Gets the default asset and variant information
+    TODO: Check for the ARCH and choose corresponding default asset
     """
     return {'asset': 'jeos-21-64', 'variant': 'JeOS.21'}
 
