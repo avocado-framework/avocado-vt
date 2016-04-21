12770a88ade45b37e40058a510980ebe43ae4f54
Cleber Rosa <crosa@redhat.com>
2016-04-21 11:03:10 -0300

    Job lock: plugin that prevents multiple runs of VT jobs
    This introduces a Pre/Post Job type of plugin, that attempts to create
    a lock file in a common location before the job is started, and remove
    the lock file after the job has finished.  The goal is to prevent
    multiple runs of avocado (with VT tests) to corrupt files such as
    guest OS images.
    
    It's still possible to have multiple parallel jobs running on the same
    machine, as long as their configuration point to different lock
    directories, which should follow different data directories, to
    prevent the original problem of data file corruption.
    
    The mechanism used for locking is a simplistic one, similar to daemon
    PID files.  It should be possible to either rewrite or improve the
    lock mechanism completely without bringing changes to the user
    expected behavior of, by default, not having multiple Avocado-VT jobs
    run at once.
    
    One other issue worth noticing is that, due to Avocado's Job internals,
    which currently lacks a proper interface for querying a Job's suite
    of tests, the use of this plugin imposes extra overhead because it
    calls `_make_test_suite()` itself.  When Avocado gets a proper interface,
    this extra call can be dropped.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ avocado_vt/plugins/vt_joblock.py
@@ -0,0 +1,120 @@
+import errno
+import logging
+import os
+import re
+import random
+import string
+import sys
+
+from avocado.core import exit_codes
+from avocado.core.settings import settings
+from avocado.plugins.base import JobPre, JobPost
+
+from ..test import VirtTest
+
+
+class VTJobLock(JobPre, JobPost):
+
+    name = 'vt-joblock'
+    description = 'Avocado-VT Job Lock/Unlock'
+
+    def __init__(self):
+        self.log = logging.getLogger("avocado.app")
+        self.has_vt_test = False
+        self.lock_dir = settings.get_value(section="plugins.vtjoblock",
+                                           key="dir",
+                                           key_type=str,
+                                           default='/tmp')
+        self.show_lock = settings.get_value(section="plugins.vtjoblock",
+                                            key="show_lock_location",
+                                            key_type=bool,
+                                            default=False)
+        self.lock_file = None
+
+    def _abort(self, message, job):
+        """
+        Aborts the Job by exiting Avocado, adding a failure to the job status
+        """
+        self.log.error(message)
+        sys.exit(exit_codes.AVOCADO_JOB_FAIL | job.exitcode)
+
+    def _set_lock(self, job):
+        if not os.path.isdir(self.lock_dir):
+            msg = ('VT Job lock directory "%s" does not exist... '
+                   'exiting...' % self.lock_dir)
+            self._abort(msg, job)
+
+        pattern = 'avocado-vt-joblock-%(jobid)s-%(uid)s-%(random)s.pid'
+        # the job unique id is already random, but, let's add yet another one
+        rand = ''.join([random.choice(string.ascii_lowercase + string.digits)
+                        for i in xrange(8)])
+        path = pattern % {'jobid': job.unique_id,
+                          'uid': os.getuid(),
+                          'random': rand}
+        path = os.path.join(self.lock_dir, path)
+
+        try:
+            with open(path, 'w') as lockfile:
+                lockfile.write("%u" % os.getpid())
+            self.lock_file = path
+        except IOError as e:
+            msg = ('Failed to create VT Job lock file. Exiting...')
+            self._abort(msg, job)
+
+        if self.show_lock:
+            self.log.info("VT LOCK    : %s", self.lock_file)
+
+    def _get_lock_file(self):
+        if not os.path.isdir(self.lock_dir):
+            return None
+
+        try:
+            files = os.listdir(self.lock_dir)
+            if not files:
+                return None
+        except OSError as e:
+            if e.errno == errno.ENOENT:
+                return None
+
+        pattern = re.compile(r'avocado-vt-joblock-[0-9a-f]{40}-[0-9]+'
+                             '-[0-9a-z]{8}\.pid')
+        for lock_file in files:
+            if pattern.match(lock_file):
+                path = os.path.join(self.lock_dir, lock_file)
+                if os.path.isfile(path):
+                    content = int(open(path, 'r').read())
+                    if int(content) > 0:
+                        return path
+        return None
+
+    def _get_lock_pid(self):
+        lockfile = self._get_lock_file()
+        if lockfile is None:
+            return 0
+        return int(open(lockfile, 'r').read())
+
+    def _lock(self, job):
+        lock_pid = self._get_lock_pid()
+        if lock_pid > 0:
+            msg = ("Avocado-VT job lock acquired by PID %u. "
+                   "Aborting..." % lock_pid)
+            self._abort(msg, job)
+        self._set_lock(job)
+
+    def _unlock(self):
+        if self.lock_file:
+            os.unlink(self.lock_file)
+
+    def pre(self, job):
+        suite = job._make_test_suite(job.args.url)
+        self.has_vt_test = any(test_factory[0] is VirtTest
+                               for test_factory in suite)
+
+        if not self.has_vt_test:
+            return
+        self._lock(job)
+
+    def post(self, job):
+        if not self.has_vt_test:
+            return
+        self._unlock()
