ca58af0f0804de85ae888aede37b21ecd5f5fb62
Wei Jiangang <weijg.fnst@cn.fujitsu.com>
2016-04-23 23:50:30 +0800

    scripts.cd_hash: Fix ImportError
    1) avocado.core.log had been merged into avocado.core.output,
       and there's no configure() any more.
       so replace it with virttest.utils_misc.LoggingConfig.configure().
    
    2) "View" had been removed for logging,
       so let the code to work with the new non-View logging approach.
    
    Signed-off-by: Wei Jiangang <weijg.fnst@cn.fujitsu.com>

--- scripts/cd_hash.py
+++ scripts/cd_hash.py
@@ -7,18 +7,20 @@ Program that calculates several hashes for a given CD image.
 
 import os
 import sys
+import logging
 import optparse
 
+from virttest.utils_misc import LoggingConfig
+
 from avocado.utils import crypto
-from avocado.core import output
-from avocado.core import log
+
 
 if __name__ == "__main__":
+    log_cfg = LoggingConfig(set_fmt=False)
+    log_cfg.configure_logging()
+
     parser = optparse.OptionParser("usage: %prog [options] [filenames]")
     options, args = parser.parse_args()
-
-    log.configure()
-    view = output.View()
 
     if args:
         filenames = args
@@ -28,19 +30,17 @@ if __name__ == "__main__":
 
     for filename in filenames:
         filename = os.path.abspath(filename)
-
         file_exists = os.path.isfile(filename)
         can_read_file = os.access(filename, os.R_OK)
         if not file_exists:
-            view.notify(event='error', msg="File %s does not exist!" % filename)
+            logging.error("File %s does not exist!", filename)
             continue
         if not can_read_file:
-            view.notify(event='error',
-                        msg="File %s does not have read permissions!" % filename)
+            logging.error("File %s does not have read permissions!", filename)
             continue
 
-        view.notify(event='message', msg="Hash values for file %s" % os.path.basename(filename))
-        view.notify(event='minor', msg="md5    (1m): %s" % crypto.hash_file(filename, 1024 * 1024, algorithm="md5"))
-        view.notify(event='minor', msg="sha1   (1m): %s" % crypto.hash_file(filename, 1024 * 1024, algorithm="sha1"))
-        view.notify(event='minor', msg="md5  (full): %s" % crypto.hash_file(filename, algorithm="md5"))
-        view.notify(event='minor', msg="sha1 (full): %s" % crypto.hash_file(filename, algorithm="sha1"))
+        logging.info("Hash values for file %s", os.path.basename(filename))
+        logging.info("    md5    (1m): %s", crypto.hash_file(filename, 1024 * 1024, algorithm="md5"))
+        logging.info("    sha1   (1m): %s", crypto.hash_file(filename, 1024 * 1024, algorithm="sha1"))
+        logging.info("    md5  (full): %s", crypto.hash_file(filename, algorithm="md5"))
+        logging.info("    sha1 (full): %s", crypto.hash_file(filename, algorithm="sha1"))
