ae2afd694dd13bdb01e23276c9f1d8dfd6e14132
Wei Jiangang <weijg.fnst@cn.fujitsu.com>
2016-04-23 23:49:05 +0800

    virttest.utils_misc: add param for __init__ of LoggingConfig
    New param 'set_fmt' indicates that allow to set Logging format or not.
    
    Signed-off-by: Wei Jiangang <weijg.fnst@cn.fujitsu.com>

--- virttest/utils_misc.py
+++ virttest/utils_misc.py
@@ -936,15 +936,17 @@ class LoggingConfig(object):
         fmt='%(asctime)s %(levelname)-5.5s| %(message)s',
         datefmt='%H:%M:%S')
 
-    def __init__(self, use_console=True):
+    def __init__(self, use_console=True, set_fmt=True):
         self.logger = logging.getLogger()
         self.global_level = logging.DEBUG
         self.use_console = use_console
+        self.set_fmt = set_fmt
 
     def add_stream_handler(self, stream, level=logging.DEBUG):
         handler = logging.StreamHandler(stream)
         handler.setLevel(level)
-        handler.setFormatter(self.console_formatter)
+        if self.set_fmt:
+            handler.setFormatter(self.console_formatter)
         self.logger.addHandler(handler)
         return handler
 
@@ -962,7 +964,8 @@ class LoggingConfig(object):
             file_path = os.path.join(log_dir, file_path)
         handler = logging.FileHandler(file_path)
         handler.setLevel(level)
-        handler.setFormatter(self.file_formatter)
+        if self.set_fmt:
+            handler.setFormatter(self.file_formatter)
         self.logger.addHandler(handler)
         return handler
 
