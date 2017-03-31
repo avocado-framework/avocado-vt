9043d263c7bd935474191dcf38fa5a24f60ce4a8
Xu Tian <xutian@redhat.com>
2017-03-31 09:37:38 +0800

    virttest.utils_misc: fix unicode decode errors
    Signed-off-by: Xu Tian <xutian@redhat.com>

--- virttest/utils_misc.py
+++ virttest/utils_misc.py
@@ -477,10 +477,9 @@ def log_line(filename, line):
                 pass
             _open_log_files[base_file] = open(log_file, "w")
         timestr = time.strftime("%Y-%m-%d %H:%M:%S")
-        _open_log_files[base_file].write("%s: %s\n"
-                                         % (timestr,
-                                            line.encode(ENCODING,
-                                                        'ignore')))
+        line = unicode("%s: %s" % (timestr, line), errors="ignore")
+        line = line.encode(ENCODING, errors='ignore')
+        _open_log_files[base_file].write("%s\n" % line)
         _open_log_files[base_file].flush()
     finally:
         _log_lock.release()
