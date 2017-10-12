ece85f39265ea6da328c886159d7589b6d0d400c
Yi Sun <yisun@redhat.com>
2017-10-12 13:51:02 +0800

    Fully cover xml attributes for scsi hostdev
    Add support for sgio, rawio, readonly, shareable attributes.
    Add support for iscsi protocol xml.
    Add support for authentication xml.
    
    Signed-off-by: Yi Sun <yisun@redhat.com>

--- virttest/libvirt_xml/devices/hostdev.py
+++ virttest/libvirt_xml/devices/hostdev.py
@@ -9,8 +9,9 @@ from virttest.libvirt_xml import accessors
 
 class Hostdev(base.TypedDeviceBase):
 
-    __slots__ = ('mode', 'hostdev_type', 'source',
-                 'managed', 'boot_order',)
+    __slots__ = ('hostdev_type', 'mode', 'managed', 'sgio', 'rawio',
+                 'source', 'boot_order', 'readonly', 'shareable'
+                 )
 
     def __init__(self, type_name="hostdev", virsh_instance=base.base.virsh):
         accessors.XMLAttribute('hostdev_type', self, parent_xpath='/',
@@ -19,13 +20,20 @@ class Hostdev(base.TypedDeviceBase):
                                tag_name='hostdev', attribute='mode')
         accessors.XMLAttribute('managed', self, parent_xpath='/',
                                tag_name='hostdev', attribute='managed')
+        accessors.XMLAttribute('sgio', self, parent_xpath='/',
+                               tag_name='hostdev', attribute='sgio')
+        accessors.XMLAttribute('rawio', self, parent_xpath='/',
+                               tag_name='hostdev', attribute='rawio')
         accessors.XMLElementNest('source', self, parent_xpath='/',
                                  tag_name='source', subclass=self.Source,
                                  subclass_dargs={
                                      'virsh_instance': virsh_instance})
         accessors.XMLAttribute('boot_order', self, parent_xpath='/',
                                tag_name='boot', attribute='order')
-
+        accessors.XMLElementBool('readonly', self, parent_xpath='/',
+                                 tag_name='readonly')
+        accessors.XMLElementBool('shareable', self, parent_xpath='/',
+                                 tag_name='shareable')
         super(self.__class__, self).__init__(device_tag='hostdev',
                                              type_name=type_name,
                                              virsh_instance=virsh_instance)
@@ -33,15 +41,30 @@ class Hostdev(base.TypedDeviceBase):
     def new_source(self, **dargs):
         new_one = self.Source(virsh_instance=self.virsh)
         if self.hostdev_type == 'pci':
-            new_address = new_one.new_untyped_address(**dargs)
-            new_one.untyped_address = new_address
-        if self.hostdev_type == 'usb':
+            pass
+        elif self.hostdev_type == 'usb':
             new_one.vendor_id = dargs.pop("vendor_id", None)
             new_one.product_id = dargs.pop("product_id", None)
-            new_address = new_one.new_untyped_address(**dargs)
-            new_one.untyped_address = new_address
-        if self.hostdev_type == 'scsi':
-            new_one.adapter_name = dargs.pop("adapter_name", None)
+        elif self.hostdev_type == 'scsi':
+            if dargs.get("adapter_name"):
+                new_one.adapter_name = dargs.pop("adapter_name")
+            if dargs.get("protocol"):
+                new_one.protocol = dargs.pop("protocol")
+            if dargs.get("source_name"):
+                new_one.source_name = dargs.pop("source_name")
+            if dargs.get("host_name"):
+                new_one.host_name = dargs.pop("host_name")
+            if dargs.get("host_port"):
+                new_one.host_port = dargs.pop("host_port")
+            auth_args = {'auth_user': dargs.pop('auth_user', None),
+                         'secret_type': dargs.pop('secret_type', None),
+                         'secret_uuid': dargs.pop('secret_uuid', None),
+                         'secret_usage': dargs.pop('secret_usage', None)
+                         }
+            if auth_args['auth_user']:
+                new_auth = new_one.new_auth(**auth_args)
+                new_one.auth = new_auth
+        if dargs:
             new_address = new_one.new_untyped_address(**dargs)
             new_one.untyped_address = new_address
         return new_one
@@ -49,7 +72,8 @@ class Hostdev(base.TypedDeviceBase):
     class Source(base.base.LibvirtXMLBase):
 
         __slots__ = ('untyped_address', 'vendor_id', 'product_id',
-                     'adapter_name')
+                     'adapter_name', 'protocol', 'source_name',
+                     'host_name', 'host_port', 'auth')
 
         def __init__(self, virsh_instance=base.base.virsh):
             accessors.XMLAttribute('vendor_id', self, parent_xpath='/',
@@ -62,13 +86,33 @@ class Hostdev(base.TypedDeviceBase):
                                          'virsh_instance': virsh_instance})
             accessors.XMLAttribute('adapter_name', self, parent_xpath='/',
                                    tag_name='adapter', attribute='name')
+            accessors.XMLAttribute('protocol', self, parent_xpath='/',
+                                   tag_name='source', attribute='protocol')
+            accessors.XMLAttribute('source_name', self, parent_xpath='/',
+                                   tag_name='source', attribute='name')
+            accessors.XMLAttribute('host_name', self, parent_xpath='/',
+                                   tag_name='host', attribute='name')
+            accessors.XMLAttribute('host_port', self, parent_xpath='/',
+                                   tag_name='host', attribute='port')
+            accessors.XMLElementNest('auth', self, parent_xpath='/',
+                                     tag_name='auth', subclass=self.Auth,
+                                     subclass_dargs={
+                                         'virsh_instance': virsh_instance})
             super(self.__class__, self).__init__(virsh_instance=virsh_instance)
             self.xml = '<source/>'
 
         def new_untyped_address(self, **dargs):
             new_one = self.UntypedAddress(virsh_instance=self.virsh)
             for key, value in dargs.items():
-                setattr(new_one, key, value)
+                if value:
+                    setattr(new_one, key, value)
+            return new_one
+
+        def new_auth(self, **dargs):
+            new_one = self.Auth(virsh_instance=self.virsh)
+            for key, value in dargs.items():
+                if value:
+                    setattr(new_one, key, value)
             return new_one
 
         class UntypedAddress(base.UntypedDeviceBase):
@@ -94,3 +138,19 @@ class Hostdev(base.TypedDeviceBase):
                 super(self.__class__, self).__init__(
                     "address", virsh_instance=virsh_instance)
                 self.xml = "<address/>"
+
+        class Auth(base.base.LibvirtXMLBase):
+
+            __slots__ = ('auth_user', 'secret_type', 'secret_uuid', 'secret_usage')
+
+            def __init__(self, virsh_instance=base.base.virsh, auth_user=""):
+                accessors.XMLAttribute('auth_user', self, parent_xpath='/',
+                                       tag_name='auth', attribute='username')
+                accessors.XMLAttribute('secret_type', self, parent_xpath='/',
+                                       tag_name='secret', attribute='type')
+                accessors.XMLAttribute('secret_uuid', self, parent_xpath='/',
+                                       tag_name='secret', attribute='uuid')
+                accessors.XMLAttribute('secret_usage', self, parent_xpath='/',
+                                       tag_name='secret', attribute='usage')
+                super(self.__class__, self).__init__(virsh_instance=virsh_instance)
+                self.xml = "<auth/>"
