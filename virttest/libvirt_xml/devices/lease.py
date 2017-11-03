2e2da1076dc78603f6f55df21782bc0e06c3d1d0
chunfuwen <chwen@redhat.com>
2017-11-03 16:11:04 +0800

    Enable lease device xml
    Element include lockspace, key and target.
    
    Signed-off-by: chunfuwen <chwen@redhat.com>

--- virttest/libvirt_xml/devices/lease.py
+++ virttest/libvirt_xml/devices/lease.py
@@ -5,9 +5,27 @@ http://libvirt.org/formatdomain.html#elementsLease
 """
 
 from virttest.libvirt_xml.devices import base
+from virttest.libvirt_xml import accessors
 
 
 class Lease(base.UntypedDeviceBase):
-    # TODO: Write this class
-    __metaclass__ = base.StubDeviceMeta
-    _device_tag = 'lease'
+    __slots__ = ('lockspace', 'key', 'target')
+
+    def __init__(self, virsh_instance=base.base.virsh):
+        # example for lease : lockspace, key and target
+        #
+        # <lease>
+        #   <lockspace>somearea</lockspace>
+        #   <key>somekey</key>
+        #   <target path='/some/lease/path' offset='1024'/>
+        # </lease>
+        accessors.XMLElementText('lockspace', self,
+                                 parent_xpath='/',
+                                 tag_name='lockspace')
+        accessors.XMLElementText('key', self,
+                                 parent_xpath='/',
+                                 tag_name='key')
+        accessors.XMLElementDict('target', self, parent_xpath='/',
+                                 tag_name='target')
+        super(Lease, self).__init__(device_tag='lease',
+                                    virsh_instance=virsh_instance)
