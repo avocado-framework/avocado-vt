avocado-framework/avocado-vt

commit da933a32204c3d6bda6638ff694b5eda7d036a1e

Python 3: Replace commands module to avocado.util.process
Signed-off-by: Junxiang Li <junli@redhat.com>

virttest/utils_net.py
@@ -8,7 +8,6 @@ import random
 import math
 import time
 import shelve
-import commands
 import signal
 import netifaces
 import netaddr
@@ -997,8 +996,8 @@ class Bridge(object):
         ebr_i = re.compile(r"^(\S+).*?(\S+)$", re.MULTILINE)
         br_i = re.compile(r"^(\S+).*?(\S+)\s+(\S+)$", re.MULTILINE)
         nbr_i = re.compile(r"^\s+(\S+)$", re.MULTILINE)
-        out_line = (process.run(r"%s show" % brctl_bin,
-                                verbose=False).stdout.splitlines())
+        out_line = (process.system_output(r"%s show" % brctl_bin,
+                                          verbose=False).splitlines())
         result = dict()
         bridge = None
 
@@ -1261,7 +1260,7 @@ def find_dnsmasq_listen_address():
     :return: List of ip where dnsmasq is listening.
     """
     cmd = "ps -Af | grep dnsmasq"
-    result = process.run(cmd).stdout
+    result = process.system_output(cmd)
     return re.findall("--listen-address (.+?) ", result, re.MULTILINE)
 
 
@@ -3096,7 +3095,8 @@ def verify_ip_address_ownership(ip, macs, timeout=20.0, devs=None):
         arping_bin = utils_path.find_command("arping")
         arping_cmd = "%s -f -c3 -w%d -I %s %s" % (arping_bin, int(timeout),
                                                   dev, ip)
-        s, o = commands.getstatusoutput(arping_cmd)
+        result = process.run(arping_cmd, shell=True)
+        s, o = (result.exit_status, result.stdout.strip())
         if s != 0:
             return False
         return bool(regex.search(o))
@@ -3119,7 +3119,7 @@ def verify_ip_address_ownership(ip, macs, timeout=20.0, devs=None):
         ip_cmd = utils_path.find_command("ip")
         ip_cmd = "%s route get %s; %s -%d route | grep default" % (
             ip_cmd, ip, ip_cmd, ip_ver)
-        output = commands.getoutput(ip_cmd)
+        output = process.system_output(ip_cmd, shell=True, ignore_status=True)
         devs = set(re.findall(r"dev\s+(\S+)", output, re.I))
     if not devs:
         logging.debug("No path to %s in route table: %s" % (ip, output))
@@ -3255,7 +3255,7 @@ def get_correspond_ip(remote_ip):
     :param remote_ip: Remote ip
     :return: Local corespond IP.
     """
-    result = process.run("ip route get %s" % (remote_ip)).stdout
+    result = process.system_output("ip route get %s" % (remote_ip))
     local_ip = re.search("src (.+)", result)
     if local_ip is not None:
         local_ip = local_ip.groups()[0]

docs/source/conf.py
@@ -16,6 +16,7 @@
 
 import os
 import sys
+import subprocess
 
 
 class DocBuildError(Exception):
@@ -27,11 +28,14 @@ class DocBuildError(Exception):
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 root_path = os.path.abspath(os.path.join("..", ".."))
 sys.path.insert(0, root_path)
-import commands
-_sphinx_apidoc = commands.getoutput('which sphinx-apidoc').strip()
+_sp = subprocess.Popen('which sphinx-apidoc', shell=True, stdout=subprocess.PIPE)
+_sphinx_apidoc = _sp.communicate()[0].strip()
 _output_dir = os.path.join(root_path, 'docs', 'source', 'api')
 _api_dir = os.path.join(root_path, 'virttest')
-_status, _output = commands.getstatusoutput("%s -o %s %s" % (_sphinx_apidoc, _output_dir, _api_dir))
+_sp = subprocess.Popen("%s -o %s %s" % (_sphinx_apidoc, _output_dir, _api_dir),
+                       shell=True, stdout=subprocess.PIPE)
+_output = _sp.communicate()[0].strip()
+_status = _sp.poll()
 if _status:
     raise DocBuildError("API rst auto generation failed: %s" % _output)
 

scripts/regression.py
@@ -9,14 +9,19 @@ compute and check regression bug.
 import os
 import sys
 import re
-import commands
 import warnings
+import subprocess
 try:
     import configparser as ConfigParser
 except ImportError:
     import ConfigParser
 
 import MySQLdb
+
+
+def getoutput(cmd):
+    sp = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    return sp.communicate()[0].strip()
 
 
 def exec_sql(cmd, conf="../../global_config.ini"):
@@ -117,12 +122,12 @@ class Sample(object):
 
             sysinfodir = os.path.join(os.path.dirname(files[0]), "../../sysinfo/")
             sysinfodir = os.path.realpath(sysinfodir)
-            cpuinfo = commands.getoutput("cat %s/cpuinfo" % sysinfodir)
-            lscpu = commands.getoutput("cat %s/lscpu" % sysinfodir)
-            meminfo = commands.getoutput("cat %s/meminfo" % sysinfodir)
-            lspci = commands.getoutput("cat %s/lspci_-vvnn" % sysinfodir)
-            partitions = commands.getoutput("cat %s/partitions" % sysinfodir)
-            fdisk = commands.getoutput("cat %s/fdisk_-l" % sysinfodir)
+            cpuinfo = getoutput("cat %s/cpuinfo" % sysinfodir)
+            lscpu = getoutput("cat %s/lscpu" % sysinfodir)
+            meminfo = getoutput("cat %s/meminfo" % sysinfodir)
+            lspci = getoutput("cat %s/lspci_-vvnn" % sysinfodir)
+            partitions = getoutput("cat %s/partitions" % sysinfodir)
+            fdisk = getoutput("cat %s/fdisk_-l" % sysinfodir)
 
             status_path = os.path.join(os.path.dirname(files[0]), "../status")
             status_file = open(status_path, 'r')
@@ -453,13 +458,13 @@ def analyze(test, sample_type, arg1, arg2, configfile):
         result_file_pattern = config.get(test, "result_file_pattern")
         cmd = 'find %s|grep "%s.*/%s"' % (directory, test, result_file_pattern)
         print cmd
-        return commands.getoutput(cmd)
+        return getoutput(cmd)
 
     if sample_type == 'filepath':
         arg1 = get_list(arg1)
         arg2 = get_list(arg2)
 
-    commands.getoutput("rm -f %s.*html" % test)
+    getoutput("rm -f %s.*html" % test)
     s1 = Sample(sample_type, arg1)
     avg1 = s1.getAvg(avg_update=avg_update)
     sd1 = s1.getSD()

shared/scripts/cmd_runner.py
@@ -5,12 +5,18 @@ information in background, redirect the outputs to log files.
 
 import threading
 import shelve
-import commands
 import re
 import os
 import sys
 import random
 import string
+import subprocess
+
+
+def getstatusoutput(cmd):
+    sp = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    output = sp.communicate()[0].strip()
+    return (sp.poll(), output)
 
 
 class Runner(object):
@@ -29,20 +35,20 @@ class Runner(object):
         fd = shelve.open(p_file)
         fd["pid"] = os.getpid()
         fd.close()
-        commands.getoutput("%s &> %s_monitor" % (m_cmd, r_path))
+        subprocess.call("%s &> %s_monitor" % (m_cmd, r_path), shell=True)
 
     def thread_kill(self, cmd, p_file):
         """
         Kill the process according to its parent pid and command
         """
         fd = shelve.open(p_file)
-        _, o = commands.getstatusoutput("pstree -p %s" % fd["pid"])
+        _, o = getstatusoutput("pstree -p %s" % fd["pid"])
         try:
             tmp = cmd.split()[0]
             pid = re.findall("%s.(\d+)" % tmp, o)[0]
         except IndexError:
             return (0, "")
-        s, o = commands.getstatusoutput("kill -9 %s" % pid)
+        s, o = getstatusoutput("kill -9 %s" % pid)
         fd.close()
         return (s, o)
 
@@ -51,7 +57,7 @@ class Runner(object):
         Test thread
         """
         self.kill_thread_flag = True
-        s, o = commands.getstatusoutput(t_cmd)
+        s, o = getstatusoutput(t_cmd)
         if s != 0:
             print "Test failed or timeout: %s" % o
         if self.kill_thread_flag:

virttest/nfs.py
@@ -5,7 +5,6 @@ nfs mount and the local nfs set up and mount.
 import re
 import os
 import logging
-import commands
 
 from avocado.utils import path
 from avocado.utils import process
@@ -286,11 +285,8 @@ class NFSClient(object):
                                                                 self.mount_dir)
         cmd = self.ssh_cmd + "'%s'" % find_mountpoint_cmd
         logging.debug("The command: %s", cmd)
-        status, output = commands.getstatusoutput(cmd)
-        if status:
-            logging.debug("The command result: <%s:%s>", status, output)
+        if process.system(cmd, verbose=True, ignore_status=True, shell=True):
             return False
-
         return True
 
     def setup(self):
@@ -412,14 +408,15 @@ class NFSClient(object):
         """
         check_mount_dir_cmd = self.ssh_cmd + "'ls -d %s'" % self.mount_dir
         logging.debug("To check if the %s exists", self.mount_dir)
-        output = commands.getoutput(check_mount_dir_cmd)
+        output = process.system_output(check_mount_dir_cmd, shell=True,
+                                       ignore_status=True)
         if re.findall("No such file or directory", output, re.M):
             mkdir_cmd = self.ssh_cmd + "'mkdir -p %s'" % self.mount_dir
             logging.debug("Prepare to create %s", self.mount_dir)
-            s, o = commands.getstatusoutput(mkdir_cmd)
-            if s != 0:
-                raise exceptions.TestFail("Failed to run %s: %s" %
-                                          (mkdir_cmd, o))
+            try:
+                process.system(mkdir_cmd, verbose=True, shell=True)
+            except process.CmdError:
+                raise exceptions.TestFail("Failed to run: %s" % mkdir_cmd)
             self.mkdir_mount_remote = True
 
         if self.params.get("firewall_to_permit_nfs", "yes") == "yes":

virttest/qemu_vm.py
@@ -9,7 +9,6 @@ import os
 import logging
 import fcntl
 import re
-import commands
 import random
 
 from functools import partial
@@ -965,40 +964,6 @@ class VM(virt_vm.BaseVM):
             devices.insert(devs)
             return devices
 
-        def add_spice_rhel5(devices, spice_params, port_range=(3100, 3199)):
-            """
-            processes spice parameters on rhel5 host.
-
-            :param spice_params - string containing spice parameters
-            :param port_range - tuple with port range, default: (3000, 3199)
-            """
-
-            if devices.has_option("spice"):
-                cmd = " -spice"
-            else:
-                return ""
-            spice_help = ""
-            if devices.has_option("spice-help"):
-                spice_help = commands.getoutput("%s -device \\?" % qemu_binary)
-            s_port = str(utils_misc.find_free_port(*port_range))
-            cmd += " port=%s" % s_port
-            for param in spice_params.split():
-                value = params.get(param)
-                if value:
-                    if bool(re.search(param, spice_help, re.M)):
-                        cmd += ",%s=%s" % (param, value)
-                    else:
-                        msg = ("parameter %s is not supported in spice. It "
-                               "only supports the following parameters:\n %s"
-                               % (param, spice_help))
-                        logging.warn(msg)
-                else:
-                    cmd += ",%s" % param
-            if devices.has_option("qxl"):
-                qxl_dev_nr = params.get("qxl_dev_nr", 1)
-                cmd += " -qxl %s" % qxl_dev_nr
-            return cmd, s_port
-
         def add_spice(spice_options, port_range=(3000, 3199),
                       tls_port_range=(3200, 3399)):
             """
@@ -1403,9 +1368,12 @@ class VM(virt_vm.BaseVM):
         qemu_binary = utils_misc.get_qemu_binary(params)
 
         self.qemu_binary = qemu_binary
-        self.qemu_version = commands.getoutput("%s -version" %
-                                               qemu_binary).split(',')[0]
-        support_cpu_model = commands.getoutput("%s -cpu \\?" % qemu_binary)
+        self.qemu_version = process.system_output("%s -version" % qemu_binary,
+                                                  ignore_status=True,
+                                                  shell=True).split(',')[0]
+        support_cpu_model = process.system_output("%s -cpu \\?" % qemu_binary,
+                                                  ignore_status=True,
+                                                  shell=True)
 
         self.last_driver_index = 0
         # init the dict index_in_use
@@ -2074,34 +2042,28 @@ class VM(virt_vm.BaseVM):
         elif params.get("display") == "nographic":
             cmd += add_nographic()
         elif params.get("display") == "spice":
-            if params.get("rhel5_spice"):
-                spice_params = params.get("spice_params")
-                ret = add_spice_rhel5(devices, spice_params)
-                tmp_cmd, spice_options["spice_port"] = ret
-                cmd += tmp_cmd
-            else:
-                spice_keys = (
-                    "spice_port", "spice_password", "spice_addr", "spice_ssl",
-                    "spice_tls_port", "spice_tls_ciphers", "spice_gen_x509",
-                    "spice_x509_dir", "spice_x509_prefix",
-                    "spice_x509_key_file", "spice_x509_cacert_file",
-                    "spice_x509_key_password", "spice_x509_secure",
-                    "spice_x509_cacert_subj", "spice_x509_server_subj",
-                    "spice_secure_channels", "spice_plaintext_channels",
-                    "spice_image_compression", "spice_jpeg_wan_compression",
-                    "spice_zlib_glz_wan_compression", "spice_streaming_video",
-                    "spice_agent_mouse", "spice_playback_compression",
-                    "spice_ipv4", "spice_ipv6", "spice_x509_cert_file",
-                    "disable_copy_paste", "spice_seamless_migration",
-                    "listening_addr"
-                )
-                for skey in spice_keys:
-                    value = params.get(skey, None)
-                    if value is not None:
-                        # parameter can be defined as empty string in Cartesian
-                        # config.  Example: spice_password =
-                        spice_options[skey] = value
-                cmd += add_spice(spice_options)
+            spice_keys = (
+                "spice_port", "spice_password", "spice_addr", "spice_ssl",
+                "spice_tls_port", "spice_tls_ciphers", "spice_gen_x509",
+                "spice_x509_dir", "spice_x509_prefix",
+                "spice_x509_key_file", "spice_x509_cacert_file",
+                "spice_x509_key_password", "spice_x509_secure",
+                "spice_x509_cacert_subj", "spice_x509_server_subj",
+                "spice_secure_channels", "spice_plaintext_channels",
+                "spice_image_compression", "spice_jpeg_wan_compression",
+                "spice_zlib_glz_wan_compression", "spice_streaming_video",
+                "spice_agent_mouse", "spice_playback_compression",
+                "spice_ipv4", "spice_ipv6", "spice_x509_cert_file",
+                "disable_copy_paste", "spice_seamless_migration",
+                "listening_addr"
+            )
+            for skey in spice_keys:
+                value = params.get(skey, None)
+                if value is not None:
+                    # parameter can be defined as empty string in Cartesian
+                    # config.  Example: spice_password =
+                    spice_options[skey] = value
+            cmd += add_spice(spice_options)
         if cmd:
             devices.insert(StrDev('display', cmdline=cmd))
 
@@ -3271,8 +3233,8 @@ class VM(virt_vm.BaseVM):
         :return: the PID of the parent shell process.
         """
         try:
-            children = commands.getoutput("ps --ppid=%d -o pid=" %
-                                          self.process.get_pid()).split()
+            cmd = "ps --ppid=%d -o pid=" % self.process.get_pid()
+            children = process.system_output(cmd, ignore_status=True).split()
             return int(children[0])
         except (TypeError, IndexError, ValueError):
             return None
@@ -3283,11 +3245,10 @@ class VM(virt_vm.BaseVM):
 
         :return: the list of qemu SPIDs
         """
-        cmd = "ls /proc/%d/task" % self.get_pid()
-        status, output = commands.getstatusoutput(cmd)
-        if status:
+        try:
+            return os.listdir("/proc/%d/task" % self.get_pid())
+        except Exception:
             return []
-        return output.split()
 
     def get_shell_pid(self):
         """

virttest/staging/utils_cgroup.py
@@ -13,7 +13,6 @@ import subprocess
 import time
 import re
 import random
-import commands
 from tempfile import mkdtemp
 
 from avocado.core import exceptions
@@ -174,7 +173,8 @@ class Cgroup(object):
         try:
             cgexec_cmd = ("cgexec -g %s:%s %s %s" %
                           (self.module, cgroup, cmd, args))
-            status, output = commands.getstatusoutput(cgexec_cmd)
+            result = process.run(cgexec_cmd, shell=True)
+            status, output = (result.exit_status, result.stdout.strip())
             return status, output
         except process.CmdError as detail:
             raise exceptions.TestFail("Execute %s in cgroup failed!\n%s" %

virttest/utils_conn.py
@@ -6,7 +6,6 @@ import logging
 import os
 import shutil
 import tempfile
-import commands
 
 import aexpect
 from avocado.utils import path
@@ -975,7 +974,8 @@ class TLSConnection(ConnectionBase):
             if os.path.exists(cert_path):
                 shutil.rmtree(cert_path)
             else:
-                status, output = commands.getstatusoutput(cmd)
+                result = process.run(cmd, shell=True, ignore_status=True)
+                status, output = (result.exit_status, result.stdout.strip())
                 if status:
                     raise ConnRmCertError(cert_path, output)
 

commit 423ca43c00fa98c9a631d81c34ee05c1bcf4884c

utils_net: Fix the issue of arping
Fix the issue introduced by commit da933a3.

Signed-off-by: Xu Han <xuhan@redhat.com>

virttest/utils_net.py
@@ -3102,9 +3102,9 @@ def verify_ip_address_ownership(ip, macs, timeout=20.0, devs=None):
         arping_bin = utils_path.find_command("arping")
         arping_cmd = "%s -f -c3 -w%d -I %s %s" % (arping_bin, int(timeout),
                                                   dev, ip)
-        result = process.run(arping_cmd, shell=True)
-        s, o = (result.exit_status, result.stdout.strip())
-        if s != 0:
+        try:
+            o = process.system_output(arping_cmd, shell=True)
+        except process.CmdError:
             return False
         return bool(regex.search(o))
 