d146c61d6ccfdf63541686f6e2920e5907588c5b
Junxiang Li <junli@redhat.com>
2018-03-20 00:51:39 -0400

    Python 3: Fix the __future__ line
    Signed-off-by: Junxiang Li <junli@redhat.com>

--- scripts/github/pulls_applied.py
+++ scripts/github/pulls_applied.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import getpass
 import datetime
-from __future__ import print_function
 
 from github import Github
 from github_issues import GithubIssues
