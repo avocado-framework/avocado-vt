1cea1d07d3d3fd0a43fb8160476c94b352242bd6
Tian Xu <tian.xux@hotmail.com>
2018-04-23 09:45:23 +0800

    Add check point verify VM status is running after resume it
    Signed-off-by: Tian Xu <tian.xux@hotmail.com>

--- virttest/qemu_vm.py
+++ virttest/qemu_vm.py
@@ -2922,7 +2922,7 @@ class VM(virt_vm.BaseVM):
                 # start guest
                 if self.monitor.verify_status("paused"):
                     if not migration_mode:
-                        self.monitor.cmd("cont")
+                        self.resume()
 
             # Update mac and IP info for assigned device
             # NeedFix: Can we find another way to get guest ip?
@@ -3982,7 +3982,7 @@ class VM(virt_vm.BaseVM):
             # self points to a dead VM object
             if not not_wait_for_migration:
                 if self.is_alive() and self.is_paused():
-                    self.monitor.cmd("cont")
+                    self.resume()
                 clone.destroy(gracefully=False)
                 if env:
                     env.unregister_vm("%s_clone" % self.name)
