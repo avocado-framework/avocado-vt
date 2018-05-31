93d930824ff8da6319b91fcdadcf916bbfd1d6a0
Srikanth Aithal <sraithal@linux.vnet.ibm.com>
2018-05-31 18:38:39 +0530

    Add disk and network hotplug stress events
    This commit adds both multi disk and multi network hotplug
    to multivms with stress running inside guest
    
    Signed-off-by: Srikanth Aithal <sraithal@linux.vnet.ibm.com>

--- virttest/utils_stress.py
+++ virttest/utils_stress.py
@@ -1,21 +1,26 @@
+import os
 import random
 import time
 import threading
+import logging
 
 from avocado.utils import cpu
 
 from virttest import virsh
 from virttest import utils_hotplug
+from virttest import utils_net
 from virttest.utils_test import libvirt
+from virttest.libvirt_xml.devices.disk import Disk
 
 
 class VMStressEvents():
+
     def __init__(self, params, env):
         """
         :param params: test param
         """
         self.host_cpu_list = cpu.cpu_online_list()
-        self.iterations = int(params.get("stress_itrs", 20))
+        self.iterations = int(params.get("stress_itrs", 1))
         self.event_sleep_time = int(params.get("event_sleep_time", 10))
         self.current_vcpu = params.get("smp", 32)
         self.max_vcpu = params.get("virsh_maxcpus", 32)
@@ -23,11 +28,29 @@ class VMStressEvents():
         self.vms = env.get_all_vms()
         self.events = params.get("stress_events", "reboot").split(',')
         self.threads = []
+        self.iface_num = params.get("iface_num", '1')
+        self.iface_type = params.get("iface_type", "network")
+        self.iface_model = params.get("iface_model", "virtio")
+        self.iface_source = eval(params.get("iface_source",
+                                            "{'network':'default'}"))
+        self.attach_option = params.get("attach_option", "")
+        self.detach_option = params.get("detach_option", "")
+        self.disk_size = params.get("virt_disk_device_size", "1")
+        self.disk_type = params.get("disk_type", "file")
+        self.disk_device = params.get("disk_device", "disk")
+        self.disk_format = params.get("disk_format", "qcow2")
+        self.device_target = params.get(
+            "virt_disk_device_target", "vda").split()
+        self.path = params.get("path", "")
+        self.device_source_names = params.get(
+            "virt_disk_device_source", "").split()
+        self.disk_driver = params.get("driver_name", "qemu")
 
     def run_threads(self):
         for vm in self.vms:
             for event in self.events:
-                self.threads.append(threading.Thread(target=self.vm_stress_events, args=(event, vm)))
+                self.threads.append(threading.Thread(
+                    target=self.vm_stress_events, args=(event, vm)))
         for thread in self.threads:
             thread.start()
 
@@ -53,7 +76,8 @@ class VMStressEvents():
             elif "emulatorpin" in event:
                 for vcpu in range(int(self.current_vcpu)):
                     result = virsh.emulatorpin(vm.name,
-                                               random.choice(self.host_cpu_list),
+                                               random.choice(
+                                                   self.host_cpu_list),
                                                **dargs)
                     if not self.ignore_status:
                         libvirt.check_exit_status(result)
@@ -75,7 +99,8 @@ class VMStressEvents():
                                 'cur_config': self.current_vcpu,
                                 'cur_live': self.max_vcpu,
                                 'guest_live': self.max_vcpu}
-                    utils_hotplug.check_vcpu_value(vm, exp_vcpu, option="--live")
+                    utils_hotplug.check_vcpu_value(
+                        vm, exp_vcpu, option="--live")
                 time.sleep(self.event_sleep_time)
                 result = virsh.setvcpus(vm.name, self.current_vcpu, "--live",
                                         **dargs)
@@ -86,8 +111,50 @@ class VMStressEvents():
                                 'cur_config': self.current_vcpu,
                                 'cur_live': self.current_vcpu,
                                 'guest_live': self.current_vcpu}
-                    utils_hotplug.check_vcpu_value(vm, exp_vcpu, option="--live")
+                    utils_hotplug.check_vcpu_value(
+                        vm, exp_vcpu, option="--live")
             elif "reboot" in event:
                 vm.reboot()
+            elif "nethotplug" in event:
+                for iface_num in range(int(self.iface_num)):
+                    logging.debug("Try to attach interface %d" % iface_num)
+                    mac = utils_net.generate_mac_address_simple()
+                    options = ("%s %s --model %s --mac %s %s" %
+                               (self.iface_type, self.iface_source['network'],
+                                self.iface_model, mac, self.attach_option))
+                    logging.debug("VM name: %s , Options for Network attach: %s", vm.name, options)
+                    ret = virsh.attach_interface(vm.name, options,
+                                                 ignore_status=True)
+                    time.sleep(self.event_sleep_time)
+                    if not self.ignore_status:
+                        libvirt.check_exit_status(ret)
+                    if self.detach_option:
+                        options = ("--type %s --mac %s %s" %
+                                   (self.iface_type, mac, self.detach_option))
+                        logging.debug("VM name: %s , Options for Network detach: %s", vm.name, options)
+                        ret = virsh.detach_interface(vm.name, options,
+                                                     ignore_status=True)
+                        if not self.ignore_status:
+                            libvirt.check_exit_status(ret)
+            elif "diskhotplug" in event:
+                for disk_num in range(len(self.device_source_names)):
+                    disk = {}
+                    disk_attach_error = False
+                    disk_name = os.path.join(self.path, vm.name, self.device_source_names[disk_num])
+                    device_source = libvirt.create_local_disk(
+                        self.disk_type, disk_name, self.disk_size, disk_format=self.disk_format)
+                    disk.update({"format": self.disk_format,
+                                 "source": device_source})
+                    disk_xml = Disk(self.disk_type)
+                    disk_xml.device = self.disk_device
+                    disk_xml.driver = {"name": self.disk_driver, "type": self.disk_format}
+                    ret = virsh.attach_disk(vm.name, disk["source"], self.device_target[disk_num], self.attach_option, debug=True)
+                    if not self.ignore_status:
+                        libvirt.check_exit_status(ret, disk_attach_error)
+                    if self.detach_option:
+                        ret = virsh.detach_disk(vm.name, self.device_target[disk_num], extra=self.detach_option)
+                        if not self.ignore_status:
+                            libvirt.check_exit_status(ret)
+                        libvirt.delete_local_disk(self.disk_type, disk_name)
             else:
                 raise NotImplementedError
