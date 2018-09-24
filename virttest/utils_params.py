c8fa76805d2db80a6faab99c5be86bdfff0c4336
Plamen Dimitrov <pdimitrov@pevogam.com>
2018-09-25 00:06:37 +0800

    Add custom parameter return types to the param utils
    This includes boolean, numerical, multi-type list, and dictionary
    parameter retrieval.
    
    Signed-off-by: Plamen Dimitrov <pdimitrov@pevogam.com>

--- virttest/utils_params.py
+++ virttest/utils_params.py
@@ -3,6 +3,7 @@ try:
     from collections import UserDict as IterableUserDict
 except ImportError:
     from UserDict import IterableUserDict
+from collections import OrderedDict
 
 from avocado.core import exceptions
 
@@ -104,3 +105,91 @@ class Params(IterableUserDict):
             if self.get(key):
                 new_dict[key] = self.get(key)
         return new_dict
+
+    def get_boolean(self, key, default=False):
+        """
+        Check if a config option is set to a default affirmation or not.
+
+        :param key: config option key
+        :type key: str
+        :param bool default: whether to assume "yes" or "no" if param
+                             does not exist (defaults to False/"no").
+        :return whether option is set to 'yes'
+        :rtype: bool
+        """
+        value = self.get(key, "yes" if default else "no")
+        if value in ("yes", "on", "true"):
+            return True
+        if value in ("no", "off", "false"):
+            return False
+        raise ValueError("Cannot get boolean parameter value for %s: %s", key, value)
+
+    def get_numeric(self, key, default=0, target_type=int):
+        """
+        Get numeric value converting to integer if necessary.
+
+        :param str key: config option key
+        :param int default: default numeric value
+        :param type target_type: numeric type to return like int or float
+        :return numerical type `target_type` converted parameter value
+        :rtype: int or float
+        """
+        return target_type(self.get(key, default))
+
+    def get_list(self, key, default="", delimiter=' ', target_type=str):
+        """
+        Get a parameter value that is a character delimited list.
+
+        :param str key: parameter whose value is list
+        :param str default: default list value
+        :param str delimiter: character to split list items
+        :param type target_type: type of each item, default is string
+        :returns: empty list if if the key is not in the parameters
+        :rtype: [str]
+
+        .. note:: This an extension to the :py:func:`Params.objects` method as
+          it allows for delimiters other than white space.
+        .. seealso:: :py:func:`param_dict`
+        """
+        param_string = self.get(key, default)
+        if not param_string:
+            return []
+        else:
+            return [target_type(entry) for entry in param_string.split(delimiter)]
+
+    def get_dict(self, key, default="", delimiter=' ', need_order=False):
+        """
+        Get a param value that has the form 'name1=value1 name2=value2 ...'.
+
+        :param str key: parameter whose value is dict
+        :param str default: default dict value
+        :param str delimiter: character to split list items
+        :param bool need_order: whether to return an OrderedDict instead of
+                                a regular dict
+        :returns: empty dict if if the key is not in the parameters, a dict
+                  or an ordered dictionary if the item order is important
+        :rtype: {str: str}
+
+        This uses :py:meth:`get_list` to convert the list entries to dict.
+        """
+        if need_order:
+            result = OrderedDict()
+        else:
+            result = dict()
+        for entry in self.get_list(key, default, delimiter):
+            index = entry.find('=')
+            if index == -1:
+                raise ValueError('failed to find "=" in "{0}" (value for {1})'
+                                 .format(entry, key))
+            result[entry[:index]] = entry[index+1:]
+        return result
+
+    def drop_dict_internals(self):
+        """
+        Drop internal keys which are not of our concern and
+        return the modified params.
+
+        :returns: parameters without the internal keys
+        :rtype: {str, str}
+        """
+        return Params({key: value for key, value in self.items() if not key.startswith("_")})
