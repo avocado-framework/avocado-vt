f4c6b806d68e7c6ea14bf1ccaeacd3153d4d5e1e
Yongxue Hong <yhong@redhat.com>
2019-06-13 19:11:58 +0800

    qemu_capabilities: Add qemu_capabilities module
    This module is to define the capabilities of vm.
    Available class:
    1. Flags: Enumerate the flags of VM capabilities.
    2. Capabilities: Representation of VM capabilities.
    
    Signed-off-by: Yongxue Hong <yhong@redhat.com>

---
+++ virttest/qemu_capabilities.py
@@ -0,0 +1,49 @@
+"""
+Module for defining the capabilities of vm.
+
+Available class:
+- Flags: Enumerate of the flags of VM capabilities.
+- Capabilities: Representation of VM capabilities.
+"""
+
+from itertools import count
+
+ID_COUNTER = count()
+
+
+def _auto_value():
+    return next(ID_COUNTER)
+
+
+class Flags(object):
+    """ Enumerate the flags of VM capabilities. """
+
+    BLOCKDEV = _auto_value()
+
+
+class Capabilities(object):
+    """ Representation of VM capabilities. """
+
+    def __init__(self):
+        self._flags = set()
+
+    def set_flag(self, flag):
+        """
+        Set the flag.
+
+        :param flag: The name of flag.
+        :type flag: Flags
+        """
+        self._flags.add(flag)
+
+    def clear_flag(self, flag):
+        """
+        Clear the flag.
+
+        :param flag: The name of flag.
+        :type flag: Flags
+        """
+        self._flags.remove(flag)
+
+    def __contains__(self, flag):
+        return flag in self._flags
