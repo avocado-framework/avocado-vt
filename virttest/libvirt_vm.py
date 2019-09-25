avocado-framework/avocado-vt

commit 0b4632014730250ebca46d31187f4be69ad0ff48

env_process: avoid changing the param `image_name` for nfs
changing the param during runtime affects the test from different
places, avoid changing the param `image_name` for vm.create() to
use nfs mounted path. Instead make use of `images_base_dir` param
to provide nfs mounted path as base path for VM to be created.

Also remove copying VM image from data_dir to nfs mounted path
from multiple places and call it from vm.create() in libvirt_vm
to avoid redundant code and simpicity.

Signed-off-by: Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>
Signed-off-by: Balamuruhan S <bala24@linux.ibm.com>

virttest/libvirt_vm.py
@@ -1195,8 +1195,9 @@ class VM(virt_vm.BaseVM):
             base_dir = image_params.get("images_base_dir",
                                         data_dir.get_data_dir())
 
+            basename = params.get("storage_type") == "nfs"
             filename = storage.get_image_filename(image_params,
-                                                  base_dir)
+                                                  base_dir, basename=basename)
             if image_params.get("use_storage_pool") == "yes":
                 filename = None
                 virt_install_cmd += add_drive(help_text,
@@ -1873,6 +1874,9 @@ class VM(virt_vm.BaseVM):
         params = self.params
         root_dir = self.root_dir
 
+        if self.params.get("storage_type") == "nfs":
+            storage.copy_nfs_image(self.params, data_dir.get_data_dir(),
+                                   basename=True)
         # Verify the md5sum of the ISO images
         for cdrom in params.objects("cdroms"):
             if params.get("medium") == "import":

virttest/storage.py
@@ -129,14 +129,13 @@ def get_image_blkdebug_filename(params, root_dir):
     return blkdebug_filename
 
 
-def get_image_filename(params, root_dir):
+def get_image_filename(params, root_dir, basename=False):
     """
     Generate an image path from params and root_dir.
 
     :param params: Dictionary containing the test parameters.
     :param root_dir: Base directory for relative filenames.
-    :param image_name: Force name of image.
-    :param image_format: Format for image.
+    :param basename: True to use only basename of image_name
 
     :note: params should contain:
            image_name -- the name of the image file, without extension
@@ -159,17 +158,18 @@ def get_image_filename(params, root_dir):
                                         image_format)
             return ceph.get_image_filename(ceph_monitor, rbd_pool_name,
                                            rbd_image_name)
-        return get_image_filename_filesytem(params, root_dir)
+        return get_image_filename_filesytem(params, root_dir, basename=basename)
     else:
         logging.warn("image_name parameter not set.")
 
 
-def get_image_filename_filesytem(params, root_dir):
+def get_image_filename_filesytem(params, root_dir, basename=False):
     """
     Generate an image path from params and root_dir.
 
     :param params: Dictionary containing the test parameters.
     :param root_dir: Base directory for relative filenames.
+    :param basename: True to use only basename of image_name
 
     :note: params should contain:
            image_name -- the name of the image file, without extension
@@ -202,6 +202,8 @@ def get_image_filename_filesytem(params, root_dir):
         return cmp(first, second)
 
     image_name = params.get("image_name", "image")
+    if basename:
+        image_name = os.path.basename(image_name)
     indirect_image_select = params.get("indirect_image_select")
     if indirect_image_select:
         re_name = image_name
@@ -243,26 +245,23 @@ def get_image_filename_filesytem(params, root_dir):
     return image_filename
 
 
-def copy_nfs_image(params, image_name, root_dir):
+def copy_nfs_image(params, root_dir, basename=False):
     """
     copy image from image_path to nfs mount dir if image is not available
     or corrupted.
 
     :param params: Test dict params
-    :param image_name: Master image name.
     :param root_dir: Base directory for relative filenames.
+    :param basename: True to use only basename of image name
     :raise: TestSetupFail if image is unavailable/corrupted
     """
-    image_format = params.get("image_format", "qcow2")
     if params.get("setup_local_nfs", "no") == "yes":
         # check for image availability in NFS shared path
         base_dir = params.get("images_base_dir", data_dir.get_data_dir())
-        dst = get_image_filename(params, base_dir)
+        dst = get_image_filename(params, base_dir, basename=basename)
         if(not os.path.isfile(dst) or
            utils_misc.get_image_info(dst)['lcounts'].lower() == "true"):
-            source = os.path.join(root_dir, "images", image_name)
-            if image_format not in source:
-                source = "%s.%s" % (source, image_format)
+            source = get_image_filename(params, root_dir)
             logging.debug("Checking for image available in image data "
                           "path - %s", source)
             # check for image availability in images data directory
@@ -524,11 +523,6 @@ class QemuImg(object):
 
                 m_image_fn = get_image_filename(params, root_dir)
                 image_fn = get_image_filename(image_params, root_dir)
-
-                # If image is not available/corrupted in nfs share location
-                # ensure it is copied before cloning
-                if params.get("storage_type") == "nfs":
-                    copy_nfs_image(params, image_name, root_dir)
                 force_clone = params.get("force_image_clone", "no")
                 if not os.path.exists(image_fn) or force_clone == "yes":
                     logging.info("Clone master image for vms.")

virttest/env_process.py
@@ -825,8 +825,6 @@ def preprocess(test, params, env):
             seLinuxBool.setup()
 
         image_name_only = os.path.basename(params["image_name"])
-        params['image_name'] = os.path.join(image_nfs.mount_dir,
-                                            image_name_only)
         for image_name in params.objects("images"):
             name_tag = "image_name_%s" % image_name
             if params.get(name_tag):

virttest/tests/unattended_install.py
@@ -1200,13 +1200,6 @@ def run(test, params, env):
         process.system(dd_cmd)
     image_name = os.path.basename(dst)
     mount_point = params.get("dst_dir")
-
-    # libvirt import of guest from NFS shared path, then copy image
-    # from image_path to nfs mount dir
-    if(params.get("virt_test_type", "qemu") == "libvirt" and
-       params.get("storage_type") == "nfs"):
-        storage.copy_nfs_image(params, image_name, vt_data_dir)
-
     if mount_point and src:
         funcatexit.register(env, params.get("type"), copy_file_from_nfs, src,
                             dst, mount_point, image_name)

commit 4856de445b502af466e5b49007abd847e3a5492e

libvirt_vm: use `nfs_mount_dir` as base_dir for NFS tests
framework picks wrong path from `image_base_dir` or `data_dir` path
for nfs tests causing VM to boot/test with wrong disk path. Instead
use `nfs_mount_dir` for VM to use appropriate image for NFS based
tests.

Fixes: commit 0b463201 and 782cc76a
Signed-off-by: Balamuruhan S <bala24@linux.ibm.com>

virttest/libvirt_vm.py
@@ -1197,12 +1197,14 @@ class VM(virt_vm.BaseVM):
                 virt_install_cmd += add_pcidevice(help_text, pci_id)
 
         for image_name in params.objects("images"):
+            basename = False
             image_params = params.object_params(image_name)
 
             base_dir = image_params.get("images_base_dir",
                                         data_dir.get_data_dir())
-
-            basename = params.get("storage_type") == "nfs"
+            if params.get("storage_type") == "nfs":
+                basename = True
+                base_dir = params["nfs_mount_dir"]
             filename = storage.get_image_filename(image_params,
                                                   base_dir, basename=basename)
             if image_params.get("use_storage_pool") == "yes":

virttest/storage.py
@@ -381,7 +381,7 @@ def copy_nfs_image(params, root_dir, basename=False):
     """
     if params.get("setup_local_nfs", "no") == "yes":
         # check for image availability in NFS shared path
-        base_dir = params.get("images_base_dir", data_dir.get_data_dir())
+        base_dir = params["nfs_mount_dir"]
         dst = get_image_filename(params, base_dir, basename=basename)
         if(not os.path.isfile(dst) or
            utils_misc.get_image_info(dst)['lcounts'].lower() == "true"):