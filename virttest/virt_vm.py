fe4e1dcb643d8a1a3ed99707c2d2497dd36f4197
Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>
2020-05-04 16:09:43 +0530

    Add a param to force use serial session in case of failure
    Currently, we do have a logic in wait_for_login to
    return serial session incase of failure if provided
    by the user through function param, this patch extends
    that functionality by adding a param which can be used
    to set, So serial session is returned in case of failure,
    as most of the testcases using wait_for_login with default
    params and we do not need to modify the testcases,
    this can be useful during certain env issues if we want to
    force use serial logins for the same test.
    
    Signed-off-by: Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>

--- virttest/virt_vm.py
+++ virttest/virt_vm.py
@@ -1187,6 +1187,8 @@ class BaseVM(object):
                     session.close()
 
         error = None
+        if not serial:
+            serial = self.params.get("force_use_serial_login", "no") == "yes"
         logging.debug("Attempting to log into '%s' (timeout %ds)",
                       self.name, timeout)
         start_time = time.time()
