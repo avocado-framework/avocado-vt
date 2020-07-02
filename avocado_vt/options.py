6c8fad3b0a0baa4576fcdc3ab0ab570a47eb5db3
Cleber Rosa <crosa@redhat.com>
2020-07-02 00:25:20 -0400

    Avocado compatibility: use options instance as given
    And rely on get_opt/set_opt as compatibility, instead of turning a
    dictionary into a Namespace() instance.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado_vt/options.py
+++ avocado_vt/options.py
@@ -16,7 +16,6 @@
 Avocado VT plugin
 """
 
-import argparse
 import logging
 import os
 
@@ -46,11 +45,7 @@ class VirtTestOptionsProcess(object):
         """
         Parses options and initializes attributes.
         """
-        # Compatibility with nrunner Avocado
-        if isinstance(options, dict):
-            self.options = argparse.Namespace(**options)
-        else:
-            self.options = options
+        self.options = options
         # Here we'll inject values from the config file.
         # Doing this makes things configurable yet the number of options
         # is not overwhelming.
