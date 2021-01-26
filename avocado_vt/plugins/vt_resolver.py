0c3a7a537b47eae40f3a72d6a025e4878e712444
Cleber Rosa <crosa@redhat.com>
2021-01-26 13:33:02 -0500

    Introduce resolver implementation
    This makes Avocado-VT compatible with Avocado's Resolver architecture,
    which is used when on calls:
    
      avocado list --resolver $REFERENCE
    
    And, when a runner implementation based on nrunner is introduced, it
    will allow it to run with:
    
      avocado run --test-runner=nrunner $REFERENCE
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ avocado_vt/plugins/vt_resolver.py
@@ -0,0 +1,48 @@
+from avocado.core.nrunner import Runnable
+from avocado.core.plugin_interfaces import Resolver
+from avocado.core.resolver import (ReferenceResolution,
+                                   ReferenceResolutionResult)
+from avocado.core.settings import settings
+
+from ..discovery import DiscoveryMixIn
+
+
+class VTResolver(Resolver, DiscoveryMixIn):
+
+    name = 'vt'
+    description = 'Test resolver for Avocado-VT tests'
+
+    def _parameters_to_runnable(self, params):
+        params = self.convert_parameters(params)
+        url = params.get('name')
+        vt_params = params.get('vt_params')
+
+        # Flatten the vt_params, discarding the attributes that are not
+        # scalars, and will not be used in the context of nrunner
+        for key in ('_name_map_file', '_short_name_map_file', 'dep'):
+            if key in vt_params:
+                del(vt_params[key])
+
+        return Runnable('avocado-vt', url, **vt_params)
+
+    def resolve(self, reference):
+        self.config = settings.as_dict()
+        try:
+            cartesian_parser = self._get_parser()
+            self._save_parser_cartesian_config(cartesian_parser)
+        except Exception as details:
+            return ReferenceResolution(reference,
+                                       ReferenceResolutionResult.ERROR,
+                                       info=details)
+
+        cartesian_parser.only_filter(reference)
+
+        runnables = [self._parameters_to_runnable(_) for _ in
+                     cartesian_parser.get_dicts()]
+        if runnables:
+            return ReferenceResolution(reference,
+                                       ReferenceResolutionResult.SUCCESS,
+                                       runnables)
+        else:
+            return ReferenceResolution(reference,
+                                       ReferenceResolutionResult.NOTFOUND)
