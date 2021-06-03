4e5c3f8c9c5482fb33852651de04a6fb0e1690b5
Haijiao Zhao <haizhao@redhat.com>
2021-06-03 16:59:35 +0800

    Update xml of memballoon device
    Add alias tag to memballoon device.
    
    Signed-off-by: Haijiao Zhao <haizhao@redhat.com>

--- virttest/libvirt_xml/devices/memballoon.py
+++ virttest/libvirt_xml/devices/memballoon.py
@@ -10,7 +10,8 @@ from virttest.libvirt_xml.devices import base
 
 class Memballoon(base.UntypedDeviceBase):
 
-    __slots__ = ('model', 'stats_period', 'address', 'driver_packed')
+    __slots__ = ('model', 'stats_period', 'address', 'alias_name',
+                 'driver_packed')
 
     def __init__(self, virsh_instance=base.base.virsh):
         accessors.XMLAttribute('model', self, parent_xpath='/',
@@ -19,6 +20,8 @@ class Memballoon(base.UntypedDeviceBase):
                                tag_name='stats', attribute='period')
         accessors.XMLElementDict('address', self, parent_xpath='/',
                                  tag_name='address')
+        accessors.XMLAttribute('alias_name', self, parent_xpath='/',
+                               tag_name='alias', attribute='name')
         accessors.XMLAttribute('driver_packed', self, parent_xpath='/',
                                tag_name='driver', attribute='packed')
         super(Memballoon, self).__init__(device_tag='memballoon',
