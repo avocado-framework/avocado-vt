dadc22986cdec93139173bcd7f5b6e56e8d86e04
Lei Yang <leiyang@redhat.com>
2021-12-29 14:10:33 +0800

    Add support for vDPA test environment
    Add support for vDPA test environment.
    
    Signed-off-by: Lei Yang <leiyang@redhat.com>

--- virttest/qemu_vm.py
+++ virttest/qemu_vm.py
@@ -47,6 +47,7 @@ from virttest import arch
 from virttest import storage
 from virttest import error_context
 from virttest import utils_vsock
+from virttest import utils_vdpa
 from virttest import error_event
 from virttest.qemu_devices import qdevices, qcontainer
 from virttest.qemu_devices.utils import DeviceError
@@ -686,12 +687,14 @@ class VM(virt_vm.BaseVM):
                     netdev_extra_params=None, tapfds=None, script=None,
                     downscript=None, vhost=None, queues=None, vhostfds=None,
                     add_queues=None, helper=None, add_tapfd=None,
-                    add_vhostfd=None, vhostforce=None):
+                    add_vhostfd=None, vhostforce=None, vhostdev=None):
             mode = None
             if nettype in ['bridge', 'network', 'macvtap']:
                 mode = 'tap'
             elif nettype == 'user':
                 mode = 'user'
+            elif nettype == 'vdpa':
+                mode = 'vhost-vdpa'
             else:
                 LOG.warning("Unknown/unsupported nettype %s" % nettype)
                 return ''
@@ -767,6 +770,8 @@ class VM(virt_vm.BaseVM):
                         cmd += (",hostfwd=tcp::%%(host_port%d)s"
                                 "-:%%(guest_port%d)s" % (i, i))
                         cmd_nd += ",hostfwd=tcp::DYN-:%%(guest_port)ds"
+            elif mode == "vhost-vdpa":
+                cmd += ",vhostdev=%s" % vhostdev
 
             if add_queues and queues:
                 cmd += ",queues=%s" % queues
@@ -2195,6 +2200,9 @@ class VM(virt_vm.BaseVM):
                         vectors = 2 * int(queues) + 2
                 else:
                     vectors = None
+                vhostdev = None
+                if nettype == "vdpa":
+                    vhostdev = utils_vdpa.get_vdpa_dev_file_by_name(nic.get('netdst'))
 
                 # Setup some exclusive parameters if we are not running a
                 # negative test.
@@ -2235,7 +2243,8 @@ class VM(virt_vm.BaseVM):
                                       bootp, redirs, netdev_id, netdev_extra,
                                       tapfds, script, downscript, vhost,
                                       queues, vhostfds, add_queues, helper,
-                                      add_tapfd, add_vhostfd, vhostforce)
+                                      add_tapfd, add_vhostfd, vhostforce,
+                                      vhostdev)
 
                 if vhostfds is None:
                     vhostfds = ""
@@ -3954,6 +3963,8 @@ class VM(virt_vm.BaseVM):
                 ids.append(utils_misc.generate_random_id())
             nic.set_if_none('tapfd_ids', ids)
 
+        elif nic.nettype == 'vdpa':
+            nic.set_if_none('netdst', params.get('netdst'))
         elif nic.nettype == 'user':
             pass  # nothing to do
         else:  # unsupported nettype
@@ -4082,6 +4093,9 @@ class VM(virt_vm.BaseVM):
                 utils_net.add_to_bridge(nic.ifname, nic.netdst)
         elif nic.nettype == 'user':
             net_backend = "user"
+        elif nic.nettype == 'vdpa':
+            net_backend = 'vhost-vdpa'
+            netdev_args["vhostdev"] = utils_vdpa.get_vdpa_dev_file_by_name(nic.netdst)
         else:  # unsupported nettype
             raise virt_vm.VMUnknownNetTypeError(self.name, nic_index_or_name,
                                                 nic.nettype)
