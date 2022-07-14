940e307d2a10dc3f8e3bf5a4299b1a15691fb170
Haijiao Zhao <haizhao@redhat.com>
2022-07-14 16:28:30 +0800

    Update unittest of interface xml test
    Signed-off-by: Haijiao Zhao <haizhao@redhat.com>

--- virttest/unittests/libvirt_xml/device_xml/test_interface_xml.py
+++ virttest/unittests/libvirt_xml/device_xml/test_interface_xml.py
@@ -10,6 +10,9 @@ XML = '''
     <mac address="00:11:22:33:44:55"/>
     <ip family='ipv4' address='172.17.2.0' prefix='24'/>
     <ip family='ipv6' address='2001:db8:ac10:fd01::' prefix='64'/>
+    <virtualport type='openvswitch'>
+      <parameters profileid='menial' interfaceid='09b11c53-8b5c-4eeb-8f00-d84eaa0aaa4f'/>
+    </virtualport>
   </interface>
     '''
 
@@ -21,7 +24,11 @@ iface_attrs = {
     'ips': [
         {'family': 'ipv4', 'address': '172.17.2.0', 'prefix': '24'},
         {'family': 'ipv6', 'address': '2001:db8:ac10:fd01::', 'prefix': '64'}
-    ]
+    ],
+    'virtualport':
+        {'parameters': {'interfaceid': '09b11c53-8b5c-4eeb-8f00-d84eaa0aaa4f',
+                        'profileid': 'menial'},
+         'type': 'openvswitch'}
 }
 
 
