avocado-framework/avocado-vt

commit ddc3e37b338850eaed5fdbc0b9600531150c5de9

net_utils function get_ip_address_by_interface gives exceptions a network interface is up without ip address configured.
Signed-off-by: Sudeesh John sudeesh@linux.vnet.ibm.com

virttest/utils_net.py
@@ -3116,12 +3116,13 @@ def get_ip_address_by_interface(ifname, ip_ver="ipv4"):
         ver = netifaces.AF_INET6
     else:
         ver = netifaces.AF_INET
-    try:
-        addr = netifaces.ifaddresses(ifname).get(ver)
+    addr = netifaces.ifaddresses(ifname).get(ver)
+    if addr is not None:
         return [a['addr'] for a in addr if not a['addr'].startswith('fe80')][0]
-    except Exception, e:
-        raise IPAddrGetError("Cannot get %s address form %s interface: %s"
-                             % (ip_ver, ifname, e))
+    else:
+        logging.warning("No IP address configured for the network interface"
+                        "%s !", ifname)
+        return None
 
 
 def get_host_ip_address(params, ip_ver="ipv4"):

commit 92e1d2d746c5e8f24c2b8d13aff1a82baada59d4

utils_net.get_ip_address_by_interface:fix a regression bug
A regression was introduced by ddc3e37, which results in unexcepted
exceptions being raised instead of the correct one during some situations.

Signed-off-by: Lei Yang <leiyang@redhat.com>

virttest/utils_net.py
@@ -3462,7 +3462,7 @@ def get_ip_address_by_interface(ifname, ip_ver="ipv4", linklocal=False):
     :param ifname: interface name
     :param ip_ver: Host IP version, ipv4 or ipv6.
     :param linklocal: Whether ip address is local or remote
-    :raise NetError: When failed to fetch IP address (ioctl raised IOError.).
+    :raise NetError: When failed to fetch IP address.
     """
     if ip_ver == "ipv6":
         ver = netifaces.AF_INET6
@@ -3470,7 +3470,14 @@ def get_ip_address_by_interface(ifname, ip_ver="ipv4", linklocal=False):
     else:
         ver = netifaces.AF_INET
         linklocal_prefix = "169.254"
-    addr = netifaces.ifaddresses(ifname).get(ver)
+    try:
+        addr = netifaces.ifaddresses(ifname).get(ver)
+    # FIXME: The kind of exceptions caught should be more specific
+    except:
+        # TODO: NetError is a very general usage,it will be changed to a
+        # more friendly way in the future
+        raise NetError(
+            "Error while retrieving IP address from interface %s." % ifname)
 
     if addr is not None:
         try: