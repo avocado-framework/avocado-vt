8541b28e5f51252521b7f31fdd017faa0686abfc
Houqi (Nick) Zuo <hzuo@redhat.com>
2023-08-16 11:17:28 +0800

    vdpa_blk.py: Add a related function about vdpa
    Signed-off-by: Houqi (Nick) Zuo <hzuo@redhat.com>

---
+++ virttest/vdpa_blk.py
@@ -0,0 +1,24 @@
+"""
+This module provides interfaces about vdpa storage backend.
+
+Available functions:
+- get_image_filename: Get the device path from vdpa.
+
+"""
+from virttest import utils_vdpa
+
+
+def get_image_filename(name):
+    """
+    Get the device path from vdpa.
+    NOTE: the vdpa protocol is a customized-private protocol
+          instead of a standard protocol.
+
+    :param name: the name of device
+    :type name: str
+    :return: The path from vdpa in vpda protocol
+             e.g: vdpa:///dev/vhost-vdpa
+    :rtype: str
+    """
+    path = utils_vdpa.get_vdpa_dev_file_by_name(name)
+    return 'vdpa://%s' % path
