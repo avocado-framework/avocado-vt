6c40d043e6e11a1214570827915ea9b21920f851
Zhenchao Liu <zhencliu@redhat.com>
2024-03-08 15:53:07 +0800

    Add storage and image management support
    Added pool_selectors and image_pool_name
    
    Signed-off-by: Zhenchao Liu <zhencliu@redhat.com>

---
+++ virttest/vt_imgr/images/qemu/qemu_virt_image/raw_qemu_virt_image.py
@@ -0,0 +1,19 @@
+from .qemu_virt_image import _QemuVirtImage
+
+
+class _RawQemuVirtImage(_QemuVirtImage):
+
+    _VIRT_IMAGE_FORMAT = "raw"
+
+    @classmethod
+    def _define_config_legacy(cls, image_name, image_params):
+        config = super()._define_config_legacy(image_name, image_params)
+        spec = config["spec"]
+        spec.update(
+            {
+                "preallocation": image_params.get("preallocated"),
+                "extent_size_hint": image_params.get("image_extent_size_hint"),
+            }
+        )
+
+        return config
