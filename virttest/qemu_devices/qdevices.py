82b362a739ac5f3ec23b7447514278232462821f
Xujun Ma <xuma@redhat.com>
2024-10-08 03:27:50 -0400

    Add device address parameter for s390x
    Signed-off-by: Xujun Ma <xuma@redhat.com>

--- virttest/qemu_devices/qdevices.py
+++ virttest/qemu_devices/qdevices.py
@@ -3080,10 +3080,13 @@ class QNoAddrCustomBus(QSparseBus):
     """
 
     def _set_device_props(self, device, addr):
-        pass
+        if self.aobject == "virtual-css":
+            device.set_param("devno", "fe.0.%s" % f"{addr[0]:04x}")
+        else:
+            pass
 
     def _update_device_props(self, device, addr):
-        pass
+        self._set_device_props(device, addr)
 
 
 class QUSBBus(QSparseBus):
