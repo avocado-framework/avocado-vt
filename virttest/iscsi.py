6909682f841e1aa89450b637f8cb65a811d22dff
Houqi (Nick) Zuo <hzuo@redhat.com>
2025-03-12 15:47:03 +0800

    iscsi.py: support one target to multi backstores
    Add Fileio class and MultiPathsIscsiLIO class to
    map fileio model and target model.
    Add MultiPathsIscsiLIOManager class to manage the
    Fileio class and MultiPathsIscsiLIO class for user.
    Add the create_iSCSI function to support the new class.
    
    Signed-off-by: Houqi (Nick) Zuo <hzuo@redhat.com>

--- virttest/iscsi.py
+++ virttest/iscsi.py
@@ -9,6 +9,8 @@ iscsi in localhost then access it.
 
 from __future__ import division
 
+import ast
+import copy
 import logging
 import os
 import re
@@ -834,5 +836,512 @@ class Iscsi(object):
                 )
             iscsi_instance = IscsiTGT(params, root_dir)
         else:
-            iscsi_instance = IscsiLIO(params, root_dir)
+            iscsi_instance = (
+                MultiPathsIscsiLIOManager(params, root_dir)
+                if not params.get("target")
+                and params.get("iscsi_targets", "")
+                and params.get("iscsi_target_backstores", "")
+                else IscsiLIO(params, root_dir)
+            )
         return iscsi_instance
+
+
+class Backstore(object):
+    iscsi_backend = None
+
+    def __init__(self):
+        pass
+
+    def create_backstore(self):
+        raise NotImplementedError
+
+    def delete_backstore(self):
+        raise NotImplementedError
+
+
+class Fileio(Backstore):
+    """
+    Model of the fileio backend in backstore.
+    """
+
+    iscsi_backend = "fileio"
+
+    def __init__(self, params, root_dir):
+        """
+        Initialize LIO backend of backstore.
+
+        :param params: parameters for LIO backend of backstore.
+        :type params: dict
+        :param root_dir: path for image
+        :type root_dir: string
+        """
+        self.initiator = None
+        self.iscsi_lun_attrs = params.get("attrs")
+        emulated_image = params.get("emulated_image")
+        self.emulated_image = (
+            emulated_image
+            if os.path.isabs(emulated_image)
+            else os.path.join(root_dir, emulated_image)
+        )
+        self.backstore_name = params.get("backstore_name")
+        self.emulated_id = ""
+        self.emulated_size = params.get("emulated_image_size")
+        self.unit = self.emulated_size[-1].upper()
+        self.emulated_size = self.emulated_size[:-1]
+        # maps K,M,G,T => (count, bs)
+        emulated_size = {
+            "K": (1, 1),
+            "M": (1, 1024),
+            "G": (1024, 1024),
+            "T": (1024, 1048576),
+        }
+        if self.unit in emulated_size:
+            block_size = emulated_size[self.unit][1]
+            size = int(self.emulated_size) * emulated_size[self.unit][0]
+            self.emulated_expect_size = block_size * size
+            self.create_cmd = "dd if=/dev/zero of=%s count=%s bs=%sK" % (
+                self.emulated_image,
+                size,
+                block_size,
+            )
+        else:
+            raise exceptions.TestError(
+                "Image size provided is not in valid"
+                " format, specify proper units [K|M|G|T]"
+            )
+
+    def _existed(self):
+        """
+        Return the whether the backstore is existed or not.
+
+        :return: True if existed.
+        :rtype: Boolean
+        """
+        cmd = "targetcli ls /backstores/%s" % self.iscsi_backend
+        output = process.run(cmd).stdout_text
+        if " %s " % self.backstore_name in output:
+            return True
+        return False
+
+    def create_backstore(self):
+        """
+        Create fileio in backstore.
+        """
+        if not self._existed():
+            # create image disk
+            if not os.path.isfile(self.emulated_image):
+                process.system("mkdir -p %s" % os.path.dirname(self.emulated_image))
+                process.system(self.create_cmd)
+            else:
+                emulated_image_size = os.path.getsize(self.emulated_image) // 1024
+                if emulated_image_size != self.emulated_expect_size:
+                    # No need to remove, rebuild is fine
+                    process.system(self.create_cmd)
+
+            # Create a backstore
+            backstore_cmd = "targetcli /backstores/%s/ create %s %s" % (
+                self.iscsi_backend,
+                self.backstore_name,
+                self.emulated_image,
+            )
+            process.run(backstore_cmd)
+
+            # Set attribute
+            if self.iscsi_lun_attrs:
+                attr_cmd = "targetcli /backstores/%s/%s set attribute %s" % (
+                    self.iscsi_backend,
+                    self.backstore_name,
+                    self.iscsi_lun_attrs,
+                )
+                process.system(attr_cmd)
+
+            # Save configuration
+            process.system("targetcli / saveconfig")
+
+    def delete_backstore(self):
+        """
+        Delete fileio in backstore.
+        """
+        if self._existed():
+            backstore_cmd = "targetcli /backstores/%s/ delete %s" % (
+                self.iscsi_backend,
+                self.backstore_name,
+            )
+            process.run(backstore_cmd)
+            # Save configuration
+            process.system("targetcli / saveconfig")
+        else:
+            LOG.info("No backstore %s found. Nothing to do!" % self.backstore_name)
+
+
+class MultiPathsIscsiLIO(IscsiLIO):
+    """
+    Iscsi class supporting multi-paths for the LIO backend.
+    """
+
+    def __init__(self, params):
+        """
+        Initialize multi-paths target in iscsi.
+        """
+        self.target = params.get("target")
+        self.export_flag = False
+        self.luns_in_target = {}
+        self.portal_ip = params.get("portal_ip", ["127.0.0.1"])
+        self.id = params.get("iscsi_thread_id", data_factory.generate_random_string(4))
+        self.iscsi_target_attrs = params.get("attrs", "")
+        self.backstores = params.get("backstores")
+
+        # CHAP AUTHENTICATION
+        self.chap_flag = False
+        self.chap_user = params.get("chap_user")
+        self.chap_passwd = params.get("chap_passwd")
+        self.enable_authentication = params.get("enable_authentication")
+        if self.chap_user and self.chap_passwd:
+            self.chap_flag = True
+
+    def get_target_id(self):
+        return NotImplementedError
+
+    def export_target(self):
+        """
+        Export target in localhost for emulated iscsi
+        """
+        selinux_mode = None
+
+        # confirm if the target exists and create iSCSI target
+        cmd = "targetcli ls /iscsi 1"
+        output = process.run(cmd).stdout_text
+        if not re.findall("%s$" % self.target, output, re.M):
+            # Set selinux to permissive mode to make sure
+            # iscsi target export successfully
+            if utils_selinux.is_enforcing():
+                selinux_mode = utils_selinux.get_status()
+                utils_selinux.set_status("permissive")
+            # Create an IQN with a target named target_name
+            target_cmd = "targetcli /iscsi/ create %s" % self.target
+            output = process.run(target_cmd).stdout_text
+            if "Created target" not in output:
+                raise exceptions.TestFail(
+                    "Failed to create target %s. (%s)" % (self.target, output)
+                )
+
+            check_portal = "targetcli /iscsi/%s/tpg1/portals ls" % self.target
+            portal_info = process.run(check_portal).stdout_text
+            if "0.0.0.0:3260" in portal_info:
+                # Delete the default IP:PORT
+                cmd = (
+                    "targetcli /iscsi/%s/tpg1/portals/ delete ip_address=0.0.0.0 ip_port=3260"
+                    % self.target
+                )
+                process.run(cmd)
+            # Add the user-defined ip
+            for ip in self.portal_ip:
+                if ip not in portal_info:
+                    portal_cmd = "targetcli /iscsi/%s/tpg1/portals/ create %s" % (
+                        self.target,
+                        ip,
+                    )
+                    output = process.run(portal_cmd).stdout_text
+                    if "Created network portal" not in output:
+                        raise exceptions.TestFail(
+                            "Failed to create portal. (%s)" % output
+                        )
+
+            # Create luns in target
+            if self.backstores:
+                for name, obj in self.backstores.items():
+                    cmd = "targetcli /iscsi/%s/tpg1/luns/ " % self.target
+                    cmd += "create /backstores/%s/%s" % (
+                        obj.iscsi_backend,
+                        obj.backstore_name,
+                    )
+                    output = process.run(cmd).stdout_text
+                    luns = re.findall(r"Created LUN (\d+).", output)
+                    if not luns:
+                        raise exceptions.TestFail("Failed to create lun. (%s)" % output)
+                    self.luns_in_target.update({luns[0]: obj.backstore_name})
+
+            # Set firewall if it's enabled
+            output = process.run("firewall-cmd --state", ignore_status=True).stdout_text
+            if re.findall("^running", output, re.M):
+                # firewall is running
+                process.system("firewall-cmd --permanent --add-port=3260/tcp")
+                process.system("firewall-cmd --reload")
+
+            # Restore selinux
+            if selinux_mode is not None:
+                utils_selinux.set_status(selinux_mode)
+
+            self.export_flag = True
+        else:
+            LOG.info("Target %s has already existed!" % self.target)
+
+        if self.chap_flag:
+            # Set CHAP authentication on the exported target
+            self.set_chap_auth_target()
+            # Set CHAP authentication for initiator to login target
+            flag = False
+            for ip in self.portal_ip:
+                flag = bool(re.findall("%s$" % self.target, iscsi_discover(ip), re.M))
+                if not flag:
+                    break
+            if flag:
+                self.set_chap_auth_initiator()
+        else:
+            attr_cmd = "targetcli /iscsi/%s/tpg1/ " % self.target
+            if self.iscsi_target_attrs:
+                attr_cmd += "set attribute %s" % self.iscsi_target_attrs
+            else:
+                attr_cmd += "set attribute %s %s %s %s" % (
+                    "authentication=0",
+                    "demo_mode_write_protect=0",
+                    "generate_node_acls=1",
+                    "cache_dynamic_acls=1",
+                )
+            output = process.run(attr_cmd).stdout_text
+            LOG.info("Define access rights: %s" % output)
+
+        # Save configuration
+        process.system("targetcli / saveconfig")
+        self.export_flag = True
+
+    def delete_target(self):
+        """
+        Delete target from host.
+        """
+        cmd = "targetcli ls /iscsi 1"
+        output = process.run(cmd).stdout_text
+        if re.findall("%s" % self.target, output, re.M):
+            del_cmd = "targetcli /iscsi delete %s" % self.target
+            process.system(del_cmd)
+
+        # Save configuration
+        process.system("targetcli / saveconfig")
+
+    def login(self):
+        """
+        Login session.
+        """
+        if self.portal_ip:
+            for ip in self.portal_ip:
+                if bool(re.findall("%s$" % self.target, iscsi_discover(ip), re.M)):
+                    iscsi_login(self.target, ip)
+
+
+class MultiPathsIscsiLIOManager(object):
+    """
+    Manager for the MultiPathsIscsiLIO.
+    """
+
+    def __init__(self, params, root_dir):
+        """
+        Init the object based on the params and root_dir.
+
+        :param params: parameters of iSCSI
+        :type params: dict
+        :param root_dir: path for image
+        :type root_dir: string
+        """
+        self._targets_mgr_created = []
+        targets = params.objects("iscsi_targets")
+        self._iscsi_targets = {}
+        for target in targets:
+            val = params.object_params(target).get("iscsi_target_options")
+            if isinstance(val, str):
+                val = ast.literal_eval(val)
+                self._iscsi_targets[val["name"]] = val
+        bss = params.objects("iscsi_target_backstores")
+        self._iscsi_target_bss = {}
+        for bs in bss:
+            val = params.object_params(bs)["iscsi_target_backstore_options"]
+            self._iscsi_target_bss[bs] = ast.literal_eval(val)
+
+        # stores the objects based on each backstores
+        self._bss_object = {}
+        for name, cfg in self._iscsi_target_bss.items():
+            cfg["backstore_name"] = name
+            self._bss_object[name] = backstore_type[cfg["backend"]](cfg, root_dir)
+
+        # stores the objects based on each targets
+        self._targets_object = {}
+        for name, cfg in self._iscsi_targets.items():
+            single_target_param = copy.deepcopy(cfg)
+            single_target_param["target"] = name
+            single_target_param["backstores"] = {}
+            binding_bss = self._iscsi_targets[name].get("binding_basckstores", [])
+            if not binding_bss:
+                LOG.info("The binding_basckstores in the target %s is None!" % name)
+            else:
+                for bs_name in binding_bss:
+                    single_target_param["backstores"].update(
+                        {bs_name: self._bss_object[bs_name]}
+                    )
+            self._targets_object[name] = MultiPathsIscsiLIO(single_target_param)
+
+    def export_target(self, targets=None):
+        """
+        Create backstores and targets.
+
+        :param targets: the targets.
+        :type targets: list
+        """
+        targets = targets if targets else self._targets_object.keys()
+        for target in targets:
+            binding_bss = self._iscsi_targets[target].get("binding_basckstores", [])
+            for name in binding_bss:
+                self._bss_object[name].create_backstore()
+            self._targets_object[target].export_target()
+            self._targets_mgr_created.append(target)
+
+    def query_targets(self, emulated_images=None):
+        """
+        Filter the targets from image name given.
+
+        :param emulated_images: the image name. If none, do NOT check emulated_image
+        :type emulated_images: list
+
+        :return: the targets in list or []
+        :rtype: list
+        """
+        if not emulated_images:
+            return self._targets_mgr_created
+
+        filtered_targets = []
+        for emu in emulated_images:
+            for target, val in self._iscsi_targets:
+                bss = val.get("binding_basckstores", [])
+                for bs in bss:
+                    if (
+                        self._iscsi_target_bss[bs]["emulated_image"] in emu
+                        and target in self._targets_mgr_created
+                    ):
+                        if target not in filtered_targets:
+                            filtered_targets.append(target)
+
+        return filtered_targets
+
+    def set_chap_auth_target(self, targets=None):
+        """
+        set up authentication information for every single initiator,
+        which provides the capability to define common login information
+        for all Endpoints in a TPG
+
+        :param targets: the targets.
+        :type targets: list
+        """
+        targets = targets if targets else self._targets_object.keys()
+        for target in targets:
+            self._targets_object[target].set_chap_auth_target()
+
+    def login(self, targets=None):
+        """
+        Login target. If target is None, login in with each target in this class.
+
+        :param targets: the targets.
+        :type targets: list
+        """
+        targets = targets if targets else self._targets_object.keys()
+        for target in targets:
+            self._targets_object[target].login()
+
+    def cleanup(self, confirmed=False, targets=None):
+        """
+        Clean up env after iscsi used.
+
+        :param targets: the targets.
+        :type targets: list
+        :param confirmed: switch for cleanup all iscsi config
+        :type confirmed: bool
+        """
+        targets = targets if targets else self._targets_object.keys()
+        for target in targets:
+            self._targets_object[target].cleanup(confirmed)
+            binding_bss = self._iscsi_targets[target].get("binding_basckstores", [])
+            if not binding_bss:
+                LOG.info(
+                    "Nothing to do since the binding_basckstores in the target %s is None!"
+                    % target
+                )
+            else:
+                for name in binding_bss:
+                    self._bss_object[name].delete_backstore()
+                self._targets_mgr_created.remove(target)
+
+    def logout(self, targets=None):
+        """
+        Logout from targets.
+
+        :param targets: the targets.
+        :type targets: list
+        """
+        targets = targets if targets else self._targets_object.keys()
+        for target in targets:
+            if self._targets_object[target].logged_in():
+                iscsi_logout(target)
+
+    def logged_in(self, target):
+        """
+        Check if the session is login or not.
+
+        :param target: the target.
+        :type target: string
+
+        :return: logged in or not
+        :rtype: bool
+        """
+        if target:
+            return self._targets_object[target].logged_in()
+        raise RuntimeError("Target is None!")
+
+    def get_device_names(self, targets=None):
+        """
+        Get device name from the targets given.
+
+        :param targets: the targets.
+        :type targets: list
+
+        :return: the mapping as the following example
+                [
+                 {"device_name": "/dev/sdd",
+                  "target": "iqn.xxxxxx:xxx",
+                  "lun_in_target": "lun0",
+                  "emulated_image": "images/basefile002"},
+                 {"device_name": "/dev/sdc",
+                  "target": "iqn.xxxxxx:xxx",
+                  "lun_in_target": "lun0",
+                  "emulated_image": "images/basefile001"}
+                ]
+        :rtype: list
+        """
+        device_name = []
+        cmd = "iscsiadm -m session -P 3"
+        outputs = process.run(cmd).stdout_text.split("Target: ")[1:]
+        pattern = r"Lun:\s*(\d+).*?disk\s+(\w+).*?running"
+        targets = targets if targets else self._targets_object.keys()
+        for target in targets:
+            for output in outputs:
+                if target + " " not in output:
+                    continue
+                m = re.findall(pattern, output, re.S)
+                for i in range(len(m)):
+                    cmd = "targetcli ls /iscsi/%s/tpg1/luns/lun%s" % (target, m[i][0])
+                    output = process.run(cmd).stdout_text
+                    bs_name = re.search(r"\[([^\s\[]+)\s*\(", output).group(1)
+                    emu_img = re.search(r"\(\s*([^)\s]+)\s*\)", output).group(1)
+                    device_name.append(
+                        {
+                            "device_name": "/dev/%s" % m[i][1],
+                            "backstore_name": bs_name,
+                            "target": target,
+                            "lun_in_target": "lun%s" % m[i][0],
+                            "emulated_image": emu_img,
+                        }
+                    )
+                break
+
+        return device_name
+
+
+backstore_type = {
+    "fileio": Fileio,
+}
