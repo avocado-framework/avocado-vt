8508e4693baab92777e1df5f9b424b4d3a3ebd1c
Zhenchao Liu <zhencliu@redhat.com>
2025-06-30 16:31:03 +0800

    Added the unified resource manager support
    The resource manager manages both resource pools and resources, it
    supplies interfaces to create, update, destroy a resource object.
    The resource pool supplies resources, e.g. the nfs pool supplies
    file-based volumes. The resource defines the smallest unit of resource
    allocation, e.g. the volume.
    
    Currently the following are supported:
      resource pool type: filesystem
      resource type: volume
    
    Signed-off-by: Zhenchao Liu <zhencliu@redhat.com>

---
+++ virttest/vt_agent/managers/resource_backings/storage/dir/dir_pool_connection.py
@@ -0,0 +1,43 @@
+import logging
+import os
+
+from avocado.utils.path import init_dir
+from vt_agent.core.data_dir import get_data_dir
+
+from ...pool_connection import _ResourcePoolConnection
+
+LOG = logging.getLogger("avocado.service." + __name__)
+
+
+class _DirPoolConnection(_ResourcePoolConnection):
+
+    _CONNECT_POOL_TYPE = "filesystem"
+
+    def __init__(self, pool_config):
+        super().__init__(pool_config)
+        root_dir = pool_config["spec"]["path"]
+        if not os.path.isabs(root_dir):
+            root_dir = os.path.abspath(os.path.join(get_data_dir(), root_dir))
+        self.root_dir = root_dir
+
+    def open(self):
+        init_dir(self.root_dir)
+        return {
+            "spec": self.root_dir,
+        }
+
+    def close(self):
+        if not os.listdir(self.root_dir):
+            os.removedirs(self.root_dir)
+
+    @property
+    def connected(self):
+        return os.path.exists(self.root_dir)
+
+    @property
+    def root_dir(self):
+        return self._root_dir
+
+    @root_dir.setter
+    def root_dir(self, root_dir):
+        self._root_dir = root_dir
