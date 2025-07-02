86a0a3f7d763d7905f5ac853579e96f9b788166d
Yongxue Hong <yhong@redhat.com>
2025-07-02 15:09:14 +0800

    vt_agent: Add vt_agent for remote task execution
    Introduces vt_agent, a new XML-RPC based agent for Avocado VT.
    
    This agent allows for remote execution of commands and services on a
    target system. It features a core RPC server, dynamic service loading,
    and basic API functions for agent control and logging.
    
    Key components include:
    - Main application entry point and argument parsing (`__main__.py`, `app/`).
    - Core server logic, service discovery, data/logging utilities (`core/`).
    - Initial set of agent APIs (`api.py`) for status, quitting, and log management.
    - Example services (`services/examples/`) to demonstrate extensibility.
    
    This provides a foundation for more advanced remote interactions
    and test automation capabilities within Avocado VT.
    
    Signed-off-by: Yongxue Hong <yhong@redhat.com>
    Co-authored-by: Xu Han <xuhan@redhat.com>
    Co-authored-by: Zhenchao Liu <zhencliu@redhat.com>

---
+++ avocado_vt/vt_agent/src/avocado_vt/vt_agent/app/__init__.py
@@ -0,0 +1,8 @@
+"""
+The `app` package for the Avocado VT Agent.
+
+This package contains modules related to the application setup,
+command-line argument parsing, and main execution command for the agent.
+"""
+
+from avocado_vt.vt_agent.app import args, cmd
---
+++ avocado_vt/vt_agent/src/avocado_vt/vt_agent/core/__init__.py
@@ -0,0 +1,8 @@
+"""
+The `core` package for the Avocado VT Agent.
+
+This package provides the central functionalities of the agent, including
+the RPC server, service management, data directory handling, and logging setup.
+"""
+
+from avocado_vt.vt_agent.core import data_dir, logger, rpc
