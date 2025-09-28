5525e5c038b16ccd3472f509cc12fb9418868893
Zhenchao Liu <zhencliu@redhat.com>
2025-09-28 17:11:58 +0800

    vt_imgr: Added qemu image management support
    Signed-off-by: Zhenchao Liu <zhencliu@redhat.com>

--- virttest/vt_imgr/logical_images/__init__.py
+++ virttest/vt_imgr/logical_images/__init__.py
@@ -1,8 +1,12 @@
-_image_classes = {}
+from .qemu import QemuLogicalImage
+
+_logical_image_classes = {
+    QemuLogicalImage.IMAGE_TYPE: QemuLogicalImage,
+}
 
 
 def get_logical_image_class(image_type):
-    return _image_classes.get(image_type)
+    return _logical_image_classes.get(image_type)
 
 
 __all__ = ["get_logical_image_class"]
