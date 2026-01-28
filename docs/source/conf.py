44733018c04931d7f567688ef60938c967912479
Cleber Rosa <crosa@redhat.com>
2026-01-28 09:36:52 -0500

    Docs: respect deprecation of get_html_theme_path()
    Sphinx build points out to the deprecation, and supposedely better
    behavior when setting the html theme and its default.
    
    This aligns this setting to the one on the Avocado side that is
    working fine.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,18 +64,7 @@ VERSION = open(version_file, "r").read().strip()
 version = VERSION
 release = VERSION
 
-# on_rtd is whether we are on readthedocs.org, this line of code grabbed from
-# docs.readthedocs.org
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    try:
-        import sphinx_rtd_theme
-
-        html_theme = "sphinx_rtd_theme"
-        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    except ImportError:
-        html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 htmlhelp_basename = "avocadodoc"
 
