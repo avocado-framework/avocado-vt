avocado-framework/avocado-vt

commit 785a92daa8ea9c1f7f44918eedf0e044250ed3c6

utils_net: fix guest net restart
The restart commad was ill-defined, missing the main command.
This would result in commands like

```
ifconfig enc1 up; -k; dhcpcd enc1
```

Fix this now by making sure the net command is added.

Signed-off-by: Sebastian Mitterle <smitterl@redhat.com>

virttest/utils_net.py
@@ -1732,13 +1732,13 @@ def restart_guest_network(
         if mac_addr:
             nic_ifname = get_linux_ifname(session, mac_addr)
             restart_cmd = "ifconfig %s up; " % nic_ifname
-            restart_cmd += "%s; " % release_flag
+            restart_cmd += "%s %s; " % (dhcp_cmd, release_flag)
             if ip_version == "ipv6":
                 restart_cmd += "%s -6 %s" % (dhcp_cmd, nic_ifname)
             else:
                 restart_cmd += "%s %s" % (dhcp_cmd, nic_ifname)
         else:
-            restart_cmd = "%s; " % release_flag
+            restart_cmd += "%s %s; " % (dhcp_cmd, release_flag)
             if ip_version == "ipv6":
                 restart_cmd += "%s -6" % dhcp_cmd
             else:

commit 58f4710bc9b63c0986e99503728e111b21d6e671

utils_net: Fix uninitialized variable 'restart_cmd'
The variable 'restart_cmd' is not initialized in the else branch. It
will cause the error like:
cannot access local variable 'restart_cmd' where it is not associated with a value

Fixes: 785a92daa (utils_net: fix guest net restart)

Signed-off-by: Han Han <hhan@redhat.com>

virttest/utils_net.py
@@ -1758,7 +1758,7 @@ def restart_guest_network(
             else:
                 restart_cmd += "%s %s" % (dhcp_cmd, nic_ifname)
         else:
-            restart_cmd += "%s %s; " % (dhcp_cmd, release_flag)
+            restart_cmd = "%s %s; " % (dhcp_cmd, release_flag)
             if ip_version == "ipv6":
                 restart_cmd += "%s -6" % dhcp_cmd
             else: