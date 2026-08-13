2ee266bf7fa4b738e86a54d2136f88c7f1eae1fb
Oliver Steffen <osteffen@redhat.com>
2026-08-13 14:05:14 +0200

    utils_netperf: is_target_running()

--- virttest/utils_netperf.py
+++ virttest/utils_netperf.py
@@ -355,7 +355,11 @@ class Netperf(object):
     def is_target_running(self, target):
         list_cmd = "ps -C %s" % target
         if self.client == "nc":
-            list_cmd = "wmic process where name='%s' list" % target
+            list_cmd = (
+                'powershell -command "Get-CimInstance Win32_Process'
+                " -Filter 'Name=''%s'''" % target
+                + ' | Format-List Name"'
+            )
         try:
             output = self.session.cmd_output_safe(list_cmd, timeout=120)
             check_reg = re.compile(r"%s" % target, re.I | re.M)
